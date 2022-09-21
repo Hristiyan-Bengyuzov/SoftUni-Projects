a81ed95e1099961a31c9648833d6f83b7dcc71b9
Hristiyan <xrisko06@abv.bg>
2022-09-21 23:26:19 +0300

    added project

---
+++ C# Development/C# Basics/First Steps In Coding/Lab/07. Projects Creation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _07._Projects_Creation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int projects = int.Parse(Console.ReadLine());
+            int neededHours = projects * 3;
+            Console.WriteLine($"The architect {name} will need {neededHours} hours to complete {projects} project/s.");
+        }
+    }
+}
