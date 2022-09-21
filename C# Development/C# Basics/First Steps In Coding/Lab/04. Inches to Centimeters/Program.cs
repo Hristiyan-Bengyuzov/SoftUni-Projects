1b15f7055ad8fca1257ae474ca70f5ff0fd9bc5d
Hristiyan <xrisko06@abv.bg>
2022-09-21 23:25:38 +0300

    added project

---
+++ C# Development/C# Basics/First Steps In Coding/Lab/04. Inches to Centimeters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _04._Inches_to_Centimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double inch = double.Parse(Console.ReadLine());
+            double sm = inch * 2.54;
+            Console.WriteLine(sm);
+        }
+    }
+}
