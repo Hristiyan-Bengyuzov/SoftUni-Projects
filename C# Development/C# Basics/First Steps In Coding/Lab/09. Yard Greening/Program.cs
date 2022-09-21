6dc9a5cd0d2eb9dac09c4dcc556efbf100a217ec
Hristiyan <xrisko06@abv.bg>
2022-09-21 23:27:26 +0300

    added project

---
+++ C# Development/C# Basics/First Steps In Coding/Lab/09. Yard Greening/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _09._Yard_Greening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double m = double.Parse(Console.ReadLine());
+            double allPrice = m * 7.61;
+            double discount = allPrice * 0.18;
+            double finalPrice = allPrice - discount;
+            Console.WriteLine($"The final price is: {finalPrice} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
