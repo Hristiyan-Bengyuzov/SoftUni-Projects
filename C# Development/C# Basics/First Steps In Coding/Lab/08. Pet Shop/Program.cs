c521dacce16909f27d572d00ab43a433b449b309
Hristiyan <xrisko06@abv.bg>
2022-09-21 23:26:37 +0300

    added project

---
+++ C# Development/C# Basics/First Steps In Coding/Lab/08. Pet Shop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _08._Pet_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dog = int.Parse(Console.ReadLine());
+            int cat = int.Parse(Console.ReadLine());
+            double dogPrice = dog * 2.5;
+            double catPrice = cat * 4;
+            double result = dogPrice + catPrice;
+            Console.WriteLine($"{result} lv.");
+        }
+    }
+}
