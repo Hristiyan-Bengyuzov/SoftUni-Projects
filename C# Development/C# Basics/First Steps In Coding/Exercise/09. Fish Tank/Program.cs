638d691a28394167b4eb6fdfb66a3c721292fd1e
Hristiyan <xrisko06@abv.bg>
2022-09-22 09:55:00 +0300

    added project

---
+++ C# Development/C# Basics/First Steps In Coding/Exercise/09. Fish Tank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _09._Fish_Tank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double procent = double.Parse(Console.ReadLine());
+            procent /= 100;
+            double volume = length * width * height;
+            double volumeInLitres = volume * 0.001;
+            double neededLitres = volumeInLitres - (volumeInLitres * procent);
+            Console.WriteLine(neededLitres);
+        }
+    }
+}
