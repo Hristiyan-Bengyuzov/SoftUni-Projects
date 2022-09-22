4570a6cf520b258068e73f03a0f35e2d727e6add
Hristiyan <xrisko06@abv.bg>
2022-09-22 09:54:41 +0300

    added project

---
+++ C# Development/C# Basics/First Steps In Coding/Exercise/08. Basketball Equipment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _08._Basketball_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yearTax = int.Parse(Console.ReadLine());
+            double kecove = yearTax - (yearTax * 0.4);
+            double ekip = kecove - (kecove * 0.2);
+            double topka = 1.0 / 4 * ekip;
+            double aksesoari = 1.0 / 5 * topka;
+            double allPrice = yearTax + kecove + ekip + topka + aksesoari;
+            Console.WriteLine(allPrice);
+        }
+    }
+}
