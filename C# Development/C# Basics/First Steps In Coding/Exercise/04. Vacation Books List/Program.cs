ac3a4dc77c725f08da76ec66f4dd388d7e9e5be9
Hristiyan <xrisko06@abv.bg>
2022-09-22 09:53:38 +0300

    added project

---
+++ C# Development/C# Basics/First Steps In Coding/Exercise/04. Vacation Books List/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04._Vacation_Books_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pageCount = int.Parse(Console.ReadLine());
+            int pagePerHour = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int result = (pageCount / pagePerHour) / days;
+            Console.WriteLine(result);
+        }
+    }
+}
