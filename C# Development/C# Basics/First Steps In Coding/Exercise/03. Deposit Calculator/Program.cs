22f6e08304ced7078034082e3fee5565f1c0653d
Hristiyan <xrisko06@abv.bg>
2022-09-22 09:53:27 +0300

    added project

---
+++ C# Development/C# Basics/First Steps In Coding/Exercise/03. Deposit Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03._Deposit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositSum = double.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            double procent = double.Parse(Console.ReadLine());
+            procent /= 100;
+            double finalSum = depositSum + n * (depositSum * procent / 12);
+            Console.WriteLine(finalSum);
+        }
+    }
+}
