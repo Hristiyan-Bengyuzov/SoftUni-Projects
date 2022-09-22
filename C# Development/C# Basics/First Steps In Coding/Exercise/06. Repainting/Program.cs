cf300fa3c45abd7a023c59b3ca25978a8c0b618f
Hristiyan <xrisko06@abv.bg>
2022-09-22 09:54:13 +0300

    added project

---
+++ C# Development/C# Basics/First Steps In Coding/Exercise/06. Repainting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06._Repainting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nailon = int.Parse(Console.ReadLine());
+            int boq = int.Parse(Console.ReadLine());
+            int razreditel = int.Parse(Console.ReadLine());
+            int chasove = int.Parse(Console.ReadLine());
+            double nailonSum = (nailon + 2) * 1.5;
+            double boqSum = (boq + 0.1 * boq) * 14.5;
+            double razreditelSum = razreditel * 5;
+            double torbichkiSum = 0.4;
+            double allSum = nailonSum + boqSum + razreditelSum + torbichkiSum;
+            double maistorSum = (allSum * 0.3) * chasove;
+            double finalSum = allSum + maistorSum;
+            Console.WriteLine(finalSum);
+        }
+    }
+}
