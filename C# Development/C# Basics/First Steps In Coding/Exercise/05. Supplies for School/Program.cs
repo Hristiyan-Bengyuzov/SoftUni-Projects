ef8603c5d7159eab9cb2f08d80edd61e931c3365
Hristiyan <xrisko06@abv.bg>
2022-09-22 09:53:57 +0300

    added project

---
+++ C# Development/C# Basics/First Steps In Coding/Exercise/05. Supplies for School/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _05._Supplies_for_School
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pens = int.Parse(Console.ReadLine());
+            int markers = int.Parse(Console.ReadLine());
+            int preparat = int.Parse(Console.ReadLine());
+            double procent = int.Parse(Console.ReadLine());
+            procent /= 100;
+            double penPrice = pens * 5.80;
+            double markerPrice = markers * 7.20;
+            double preparatPrice = preparat * 1.20;
+            double allPrice = penPrice + markerPrice + preparatPrice;
+            double discountPrice = procent * (allPrice);
+            double finalPrice = allPrice - discountPrice;
+            Console.WriteLine(finalPrice);
+        }
+    }
+}
