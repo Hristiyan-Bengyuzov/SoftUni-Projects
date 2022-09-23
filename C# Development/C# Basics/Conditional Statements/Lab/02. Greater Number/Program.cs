cd4b182b2f0eae124c8710a6f49d1c0a2759b3e1
Hristiyan <xrisko06@abv.bg>
2022-09-23 21:04:29 +0300

    added project

---
+++ C# Development/C# Basics/Conditional Statements/Lab/02. Greater Number/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02._Greater_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int max = (a > b) ? a : b;
+            Console.WriteLine(max);
+        }
+    }
+}
