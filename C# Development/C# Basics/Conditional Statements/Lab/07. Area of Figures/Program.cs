d32db0185aef643e1a3a0ed13a059ab757ae749f
Hristiyan <xrisko06@abv.bg>
2022-09-23 21:05:39 +0300

    added project

---
+++ C# Development/C# Basics/Conditional Statements/Lab/07. Area of Figures/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _07._Area_of_Figures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            double area;
+            if (figure == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                area = a * a;
+            }
+            else if (figure == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                area = a * b;
+            }
+            else if (figure == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                area = Math.PI * r * r;
+            }
+            else
+            {
+                double a = double.Parse(Console.ReadLine());
+                double ha = double.Parse(Console.ReadLine());
+                area = (a * ha) / 2;
+            }
+            Console.WriteLine($"{area:f3}");
+        }
+    }
+}
