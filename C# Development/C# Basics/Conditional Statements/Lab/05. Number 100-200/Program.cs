934ae7260de43027f70d76cafbca2f10fe942d3f
Hristiyan <xrisko06@abv.bg>
2022-09-23 21:05:13 +0300

    added project

---
+++ C# Development/C# Basics/Conditional Statements/Lab/05. Number 100-200/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05._Number_100_200
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            if (x < 100)
+            {
+                Console.WriteLine("Less than 100");
+            }
+            else if (x >= 100 && x <= 200)
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+            else
+                Console.WriteLine("Greater than 200");
+        }
+    }
+}
