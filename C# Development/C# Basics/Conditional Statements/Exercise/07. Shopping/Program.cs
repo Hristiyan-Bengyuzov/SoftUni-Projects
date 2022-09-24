8195081c3d369243563d8a40ba584be7c0cbf13b
Hristiyan <xrisko06@abv.bg>
2022-09-24 09:45:36 +0300

    added project

---
+++ C# Development/C# Basics/Conditional Statements/Exercise/07. Shopping/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07._Shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int P = int.Parse(Console.ReadLine());
+            double videoCardPrice = N * 250;
+            double cpuPrice = M * (0.35 * videoCardPrice);
+            double ramPrice = P * (0.1 * videoCardPrice);
+            double allPrice = videoCardPrice + cpuPrice + ramPrice;
+            if (N > M)
+            {
+                allPrice = allPrice - (0.15 * allPrice);
+            }
+
+            Console.WriteLine(budget >= allPrice ? $"You have {budget - allPrice:f2} leva left!" : $"Not enough money! You need {allPrice - budget:f2} leva more!");
+        }
+    }
+}
