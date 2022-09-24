7e6591c347119e5f71b6b6e36645e1fa071faedb
Hristiyan <xrisko06@abv.bg>
2022-09-24 09:45:02 +0300

    added project

---
+++ C# Development/C# Basics/Conditional Statements/Exercise/05. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05._Godzilla_vs._Kong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int statistiCount = int.Parse(Console.ReadLine());
+            double clothesPricePerStatist = double.Parse(Console.ReadLine());
+            double clothesPrice = statistiCount * clothesPricePerStatist;
+            double decorPrice = budget * 0.1;
+            if (statistiCount > 150)
+            {
+                clothesPrice = clothesPrice - (clothesPrice * 0.1);
+            }
+            double finalPrice = clothesPrice + decorPrice;
+            if (finalPrice > budget)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {finalPrice - budget:f2} leva more.");
+            }
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {budget - finalPrice:f2} leva left.");
+            }
+        }
+    }
+}
