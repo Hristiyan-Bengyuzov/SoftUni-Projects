c2355a57522904fa39160c9057b9fe7597dfc2b3
Hristiyan <xrisko06@abv.bg>
2022-09-24 09:44:38 +0300

    added project

---
+++ C# Development/C# Basics/Conditional Statements/Exercise/03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03._Time___15_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int allTime = h * 60 + m + 15;
+            int hour = allTime / 60;
+            int minutes = allTime % 60;
+            if (hour == 24)
+            {
+                hour = 0;
+            }
+
+            Console.WriteLine(minutes < 10 ? $"{hour}:0{minutes}" : $"{hour}:{minutes}");
+        }
+    }
+}
