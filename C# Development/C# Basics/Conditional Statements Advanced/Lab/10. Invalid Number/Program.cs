8c22d610d6318c1f6069ee4e7fd3fab331780479
Hris <xrisko06@abv.bg>
2022-09-25 22:37:23 +0300

    added project

---
+++ C# Development/C# Basics/Conditional Statements Advanced/Lab/10. Invalid Number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _10._Invalid_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool valid = (n >= 100 && n <= 200 || n == 0);
+            if (!valid)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
