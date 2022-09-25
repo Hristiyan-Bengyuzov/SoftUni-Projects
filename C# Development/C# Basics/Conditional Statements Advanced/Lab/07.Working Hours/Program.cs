0364f64137f6c73dc74eefb046c047c8b5a8714a
Hris <xrisko06@abv.bg>
2022-09-25 22:36:59 +0300

    added project

---
+++ C# Development/C# Basics/Conditional Statements Advanced/Lab/07.Working Hours/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _07.Working_Hours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+            bool inRange = (h >= 10 && h <= 18);
+            Console.WriteLine(day == "Sunday" || !inRange ? "closed" : "open");
+        }
+    }
+}
