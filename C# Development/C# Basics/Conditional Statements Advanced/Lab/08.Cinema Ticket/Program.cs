9acc3ca31a88b9538d0bb6b13516ced460d04f9c
Hris <xrisko06@abv.bg>
2022-09-25 22:37:06 +0300

    added project

---
+++ C# Development/C# Basics/Conditional Statements Advanced/Lab/08.Cinema Ticket/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _08.Cinema_Ticket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+            int ticket = day switch
+            {
+                "Monday" => 12,
+                "Tuesday" => 12,
+                "Friday" => 12,
+                "Wednesday" => 14,
+                "Thursday" => 14,
+                "Saturday" => 16,
+                "Sunday" => 16
+            };
+
+            Console.WriteLine(ticket);
+        }
+    }
+}
