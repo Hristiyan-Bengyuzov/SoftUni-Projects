ee8e0f5eecadf6553431793f90a0d46e278cf502
Hris <xrisko06@abv.bg>
2022-09-25 22:36:33 +0300

    added project

---
+++ C# Development/C# Basics/Conditional Statements Advanced/Lab/04. Personal Titles/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _04._Personal_Titles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            if (age >= 16)
+            {
+                if (gender == "m")
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else
+                    Console.WriteLine("Ms.");
+            }
+            else
+            {
+                if (gender == "m")
+                {
+                    Console.WriteLine("Master");
+                }
+                else
+                    Console.WriteLine("Miss");
+            }
+        }
+    }
+}
