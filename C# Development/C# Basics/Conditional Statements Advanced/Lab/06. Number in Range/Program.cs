68ce62428bfcc349ebea47c038bf9e32827e5e48
Hris <xrisko06@abv.bg>
2022-09-25 22:36:50 +0300

    added project

---
+++ C# Development/C# Basics/Conditional Statements Advanced/Lab/06. Number in Range/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _06._Number_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            bool inRange = (n >= -100 && n <= 100 && n != 0);
+            Console.WriteLine(inRange ? "Yes" : "No");
+        }
+    }
+}
