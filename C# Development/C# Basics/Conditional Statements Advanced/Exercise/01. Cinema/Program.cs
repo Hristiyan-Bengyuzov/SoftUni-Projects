0c9e32f38927ad8d916c2e1938710a59f7616c33
Hris <xrisko06@abv.bg>
2022-09-26 22:43:19 +0300

    added project

---
+++ C# Development/C# Basics/Conditional Statements Advanced/Exercise/01. Cinema/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01._Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kino = Console.ReadLine();
+            int r = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            double profit = kino switch
+            {
+                "Premiere" => 12 * r * c,
+                "Normal" => 7.5 * r * c,
+                _ => 5 * r * c
+            };
+
+            Console.WriteLine($"{profit:f2} leva");
+        }
+    }
+}
