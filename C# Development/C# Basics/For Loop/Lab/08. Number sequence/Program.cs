811e221f40fc2bc196c9e83ff1fa795b473b8f23
Hris <xrisko06@abv.bg>
2022-09-27 12:04:57 +0300

    added project

---
+++ C# Development/C# Basics/For Loop/Lab/08. Number sequence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08._Number_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number >= max)
+                {
+                    max = number;
+                }
+                if (number <= min)
+                {
+                    min = number;
+                }
+            }
+            Console.WriteLine($"Max number: {max}");
+            Console.WriteLine($"Min number: {min}");
+        }
+    }
+}
