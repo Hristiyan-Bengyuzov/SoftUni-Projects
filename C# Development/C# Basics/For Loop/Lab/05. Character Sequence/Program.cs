2fdfb1f4f8271c7e1b5be2f7129d3183fd9f2510
Hris <xrisko06@abv.bg>
2022-09-27 12:04:20 +0300

    added project

---
+++ C# Development/C# Basics/For Loop/Lab/05. Character Sequence/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _05._Character_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.WriteLine(word[i]);
+            }
+        }
+    }
+}
