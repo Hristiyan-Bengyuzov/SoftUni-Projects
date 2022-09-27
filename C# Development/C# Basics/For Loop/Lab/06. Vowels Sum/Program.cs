81c848cec15359dd9028223f984fd11085793d6e
Hris <xrisko06@abv.bg>
2022-09-27 12:04:32 +0300

    added project

---
+++ C# Development/C# Basics/For Loop/Lab/06. Vowels Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06._Vowels_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int sum = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                char letter = word[i];
+                sum = letter switch
+                {
+                    'a' => sum + 1,
+                    'e' => sum + 2,
+                    'i' => sum + 3,
+                    'o' => sum + 4,
+                    'u' => sum + 5,
+                    _ => sum
+                };
+                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
