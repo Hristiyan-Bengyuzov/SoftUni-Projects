f0bf2bb401457286013dad7cd6492de8d2f7b890
Hris <xrisko06@abv.bg>
2022-09-28 10:02:52 +0300

    added project

---
+++ C# Development/C# Basics/For Loop/Exercise/05. Salary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05._Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int openTabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+            for (int i = 0; i < openTabs; i++)
+            {
+                string tab = Console.ReadLine();
+                salary -= tab switch
+                {
+                    "Facebook" => 150,
+                    "Instagram" => 100,
+                    "Reddit" => 50,
+                    _ => 0
+                };
+
+                if (salary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    return;
+                }
+            }
+
+            Console.WriteLine(salary);
+        }
+    }
+}
