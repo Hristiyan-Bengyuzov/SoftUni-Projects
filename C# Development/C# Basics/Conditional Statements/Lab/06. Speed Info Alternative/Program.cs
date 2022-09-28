c51ea240572d47961a316a3ca5f8fd8761b25b54
Hris <xrisko06@abv.bg>
2022-09-28 22:03:33 +0300

    added alternative with switch expression

---
+++ C# Development/C# Basics/Conditional Statements/Lab/06. Speed Info Alternative/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _06._Speed_Info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double speed = double.Parse(Console.ReadLine());
+            Console.WriteLine(speed switch
+            {
+                <= 10 => "slow",
+                <= 50 => "average",
+                <= 150 => "fast",
+                <= 1000 => "ultra fast",
+                _ => "extremely fast"
+            });
+        }
+    }
+}
