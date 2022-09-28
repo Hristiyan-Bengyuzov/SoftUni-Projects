832acbff40cd1a7c4b17cfaa094f1e46d933a124
Hris <xrisko06@abv.bg>
2022-09-28 10:03:20 +0300

    added project

---
+++ C# Development/C# Basics/For Loop/Exercise/07. Trekking Mania/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _07._Trekking_Mania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int groups = int.Parse(Console.ReadLine());
+            double musalaProcent = 0.0;
+            double monblanProcent = 0.0;
+            double kilimanjaroProcent = 0.0;
+            double k2Procent = 0.0;
+            double everestProcent = 0.0;
+            int allClimbers = 0;
+            for (int i = 0; i < groups; i++)
+            {
+                int climbers = int.Parse(Console.ReadLine());
+                allClimbers += climbers;
+                if (climbers <= 5)
+                {
+                    musalaProcent += climbers;
+                }
+                else if (climbers <= 12)
+                {
+                    monblanProcent += climbers;
+                }
+                else if (climbers <= 25)
+                {
+                    kilimanjaroProcent += climbers;
+                }
+                else if (climbers <= 40)
+                {
+                    k2Procent += climbers;
+                }
+                else
+                {
+                    everestProcent += climbers;
+                }
+            }
+
+            Console.WriteLine($"{musalaProcent / allClimbers * 100:f2}%");
+            Console.WriteLine($"{monblanProcent / allClimbers * 100:f2}%");
+            Console.WriteLine($"{kilimanjaroProcent / allClimbers * 100:f2}%");
+            Console.WriteLine($"{k2Procent / allClimbers * 100:f2}%");
+            Console.WriteLine($"{everestProcent / allClimbers * 100:f2}%");
+        }
+    }
+}
