b872e64c994cf9e4bc59701ef3dfb9df5dbb2755
Hris <xrisko06@abv.bg>
2022-09-29 08:55:25 +0300

    added project

---
+++ C# Development/C# Basics/While Loop/Lab/04. Sequence 2k+1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04._Sequence_2k_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int currentNumber = 1;
+
+            while (currentNumber <= number)
+            {
+                Console.WriteLine(currentNumber);
+                currentNumber = currentNumber * 2 + 1;
+            }
+        }
+    }
+}
