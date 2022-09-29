cdc9d2ad94dee0534ff86425aee3d79062a92206
Hris <xrisko06@abv.bg>
2022-09-29 08:55:45 +0300

    added project

---
+++ C# Development/C# Basics/While Loop/Lab/06. Max Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06._Max_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int max = int.MinValue;
+
+            while (input != "Stop")
+            {
+                int currentNumber = int.Parse(input);
+                if (currentNumber > max)
+                {
+                    max = currentNumber;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
