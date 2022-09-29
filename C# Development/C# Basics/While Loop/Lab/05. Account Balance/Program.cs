25f159a88eb548fe24d8dfa164aadaa633c6d37a
Hris <xrisko06@abv.bg>
2022-09-29 08:55:35 +0300

    added project

---
+++ C# Development/C# Basics/While Loop/Lab/05. Account Balance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05._Account_Balance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double sum = 0;
+
+            while (input != "NoMoreMoney")
+            {
+                double currentNumber = double.Parse(input);
+
+                if (currentNumber < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+
+                sum += currentNumber;
+                Console.WriteLine($"Increase: {currentNumber:f2}");
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total: {sum:f2}");
+        }
+    }
+}
