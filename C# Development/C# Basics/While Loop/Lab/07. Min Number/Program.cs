c3a989bb452bceff02c754f33c51e6e121af6c44
Hris <xrisko06@abv.bg>
2022-09-29 08:55:56 +0300

    added project

---
+++ C# Development/C# Basics/While Loop/Lab/07. Min Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07._Min_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int min = int.MaxValue;
+
+            while (input != "Stop")
+            {
+                int currentNumber = int.Parse(input);
+                if (currentNumber < min)
+                {
+                    min = currentNumber;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(min);
+        }
+    }
+}
