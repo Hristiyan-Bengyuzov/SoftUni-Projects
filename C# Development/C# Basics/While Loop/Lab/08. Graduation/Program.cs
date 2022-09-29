73459e908c24d62fd71f8825f363b129ecba27d2
Hris <xrisko06@abv.bg>
2022-09-29 08:56:07 +0300

    added project

---
+++ C# Development/C# Basics/While Loop/Lab/08. Graduation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _08._Graduation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int grade = 0;
+            int counter = 0;
+            double sum = 0;
+
+            while (grade < 12)
+            {
+                double yearlyGrade = double.Parse(Console.ReadLine());
+              
+                if (yearlyGrade < 4)
+                {
+                    counter++;
+                }
+
+                if (counter > 1)
+                {
+                    Console.WriteLine($"{name} has been excluded at {grade} grade");
+                    return;
+                }
+
+                grade++;
+                sum += yearlyGrade;
+            }
+
+            Console.WriteLine($"{name} graduated. Average grade: {sum / grade:f2}");
+        }
+    }
+}
