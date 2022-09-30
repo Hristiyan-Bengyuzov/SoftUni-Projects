cb76f209843a76a5d9332e343b5ca2999ae03bdb
Hris <xrisko06@abv.bg>
2022-09-30 21:22:03 +0300

    added project

---
+++ C# Development/C# Basics/While Loop/Exercise/06. Cake/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cakeWidth = int.Parse(Console.ReadLine());
+            int cakeLength = int.Parse(Console.ReadLine());
+            int cakePieces = cakeWidth * cakeLength;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "STOP")
+                {
+                    Console.WriteLine($"{cakePieces} pieces are left.");
+                    return;
+                }
+
+                int currentPiece = int.Parse(command);
+
+                if (cakePieces >= currentPiece) // enough cake
+                {
+                    cakePieces -= currentPiece;
+                }
+                else
+                {
+                    Console.WriteLine($"No more cake left! You need {currentPiece - cakePieces} pieces more.");
+                    return;
+                }
+            }
+        }
+    }
+}
