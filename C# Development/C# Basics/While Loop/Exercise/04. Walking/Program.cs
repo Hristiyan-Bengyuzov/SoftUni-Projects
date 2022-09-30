9ae416e944e5b1f1db4e78a622beeb95a34e50c1
Hris <xrisko06@abv.bg>
2022-09-30 21:20:26 +0300

    added project

---
+++ C# Development/C# Basics/While Loop/Exercise/04. Walking/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _04._Walking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int GOAL = 10000;
+            int sum = 0;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Going home")
+                {
+                    int finalSteps = int.Parse(Console.ReadLine());
+                    sum += finalSteps;
+
+                    if (sum < GOAL)
+                    {
+                        Console.WriteLine($"{GOAL - sum} more steps to reach goal.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Goal reached! Good job!");
+                        Console.WriteLine($"{sum - GOAL} steps over the goal!");
+                    }
+                    return;
+                }
+
+                int currentSteps = int.Parse(command);
+                sum += currentSteps;
+
+                if (sum >= GOAL)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                    Console.WriteLine($"{sum - GOAL} steps over the goal!");
+                    return;
+                }
+            }
+        }
+    }
+}
