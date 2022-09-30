a0d211d0111872198cd1e90a87dc12dca23007ec
Hris <xrisko06@abv.bg>
2022-09-30 21:30:00 +0300

    removed redundant line

--- C# Development/C# Basics/For Loop/Lab/04. Even Powers of 2/Program.cs
+++ C# Development/C# Basics/For Loop/Lab/04. Even Powers of 2/Program.cs
@@ -2,19 +2,17 @@
 
 namespace _04._Even_Powers_of_2
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            Console.WriteLine(1);
-            for (int i = 1; i <= n; i++)
+            for (int i = 0; i <= n; i++)
             {
                 if (i % 2 == 0)
                 {
                     Console.WriteLine(Math.Pow(2, i));
                 }
-
             }
         }
     }
