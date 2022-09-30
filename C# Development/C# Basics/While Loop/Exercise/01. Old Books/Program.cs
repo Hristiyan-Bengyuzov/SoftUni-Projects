10bf114321d7ec83b7044458754c5b7271d59bd2
Hris <xrisko06@abv.bg>
2022-09-30 21:19:58 +0300

    added project

---
+++ C# Development/C# Basics/While Loop/Exercise/01. Old Books/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01._Old_Books
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wantedBook = Console.ReadLine();
+            int checkedBooks = 0;
+
+            while (true)
+            {
+                string currentBook = Console.ReadLine();
+
+                if (currentBook == "No More Books")
+                {
+                    break;
+                }
+
+                if (currentBook == wantedBook)
+                {
+                    Console.WriteLine($"You checked {checkedBooks} books and found it.");
+                    return;
+                }
+
+                checkedBooks++;
+            }
+
+            Console.WriteLine("The book you search is not here!");
+            Console.WriteLine($"You checked {checkedBooks} books.");
+        }
+    }
+}
