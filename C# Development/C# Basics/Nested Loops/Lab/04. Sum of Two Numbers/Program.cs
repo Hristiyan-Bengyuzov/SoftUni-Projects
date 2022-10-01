3a3c165541f42f56ee8cc8fd4e1eb28acd043a91
Hris <xrisko06@abv.bg>
2022-10-01 23:05:00 +0300

    added project

---
+++ C# Development/C# Basics/Nested Loops/Lab/04. Sum of Two Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _04._Sum_of_Two_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int result = int.Parse(Console.ReadLine());
+
+            int allCombinations = 0;
+
+            for (int i = x; i <= y; i++)
+            {
+                for (int j = x; j <= y; j++)
+                {
+                    allCombinations++;
+                    if (i + j == result)
+                    {
+                        Console.WriteLine($"Combination N:{allCombinations} ({i} + {j} = {result})");
+                        return;
+                    }
+                }
+
+            }
+
+            Console.WriteLine($"{allCombinations} combinations - neither equals {result}");
+        }
+    }
+}
