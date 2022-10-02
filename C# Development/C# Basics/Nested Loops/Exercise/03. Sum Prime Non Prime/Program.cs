7858c7e0cbd03ff3f56d277d9c68b34b918cf196
Hris <xrisko06@abv.bg>
2022-10-02 23:32:23 +0300

    added project

---
+++ C# Development/C# Basics/Nested Loops/Exercise/03. Sum Prime Non Prime/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _03._Sum_Prime_Non_Prime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int primeSum = 0;
+            int nonPrimeSum = 0;
+
+            while (input != "stop")
+            {
+                int num = int.Parse(input);
+
+                if (num < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                if (IsPrime(num))
+                {
+                    primeSum += num;
+                }
+                else
+                {
+                    nonPrimeSum += num;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Sum of all prime numbers is: {primeSum}");
+            Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeSum}");
+        }
+
+        static bool IsPrime(int num)
+        {
+            if (num <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
