5df2336701531b6c92955060e8eca3e3b40a7325
Hris <xrisko06@abv.bg>
2022-10-03 22:43:49 +0300

    added project

---
+++ C# Development/C# Fundamentals/Basic Syntax, Conditional Statements and Loops/Lab/05. Month Printer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05._Month_Printer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(month switch
+            {
+                1 => "January",
+                2 => "February",
+                3 => "March",
+                4 => "April",
+                5 => "May",
+                6 => "June",
+                7 => "July",
+                8 => "August",
+                9 => "September",
+                10 => "October",
+                11 => "November",
+                12 => "December",
+                _ => "Error!"
+            });
+        }
+    }
+}
