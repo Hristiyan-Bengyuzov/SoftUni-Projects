309a8963804d20d5279d7348fb36ba4acabfc22f
Hris <xrisko06@abv.bg>
2022-10-03 22:44:27 +0300

    added project

---
+++ C# Development/C# Fundamentals/Basic Syntax, Conditional Statements and Loops/Lab/10. Multiplication Table/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _10._Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int times = 1;
+
+            while (times <= 10)
+            {
+                Console.WriteLine($"{n} X {times} = {n * times}");
+                times++;
+            }
+        }
+    }
+}
