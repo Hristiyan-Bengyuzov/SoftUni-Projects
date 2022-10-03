6b5fe1d9801b7a6e1ce5e81fb171f851415ccdb4
Hris <xrisko06@abv.bg>
2022-10-03 22:43:33 +0300

    added project

---
+++ C# Development/C# Fundamentals/Basic Syntax, Conditional Statements and Loops/Lab/03. Passed or Failed/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _03._Passed_or_Failed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            Console.WriteLine(grade >= 3 ? "Passed!" : "Failed!");
+        }
+    }
+}
