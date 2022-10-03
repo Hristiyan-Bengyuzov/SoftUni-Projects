8ecb7868d815ebb00cd0ce0d30e18ec40ab45bd1
Hris <xrisko06@abv.bg>
2022-10-03 22:44:19 +0300

    added project

---
+++ C# Development/C# Fundamentals/Basic Syntax, Conditional Statements and Loops/Lab/09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _09._Sum_of_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int currentOddNumber = 2 * i - 1;
+                Console.WriteLine(currentOddNumber);
+                sum += currentOddNumber;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
