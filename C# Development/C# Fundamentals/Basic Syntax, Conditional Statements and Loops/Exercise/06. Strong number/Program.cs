bb897e0a0e602661006e70d430ccae02f4b09d72
Hris <xrisko06@abv.bg>
2022-10-06 21:09:46 +0300

    added project

---
+++ C# Development/C# Fundamentals/Basic Syntax, Conditional Statements and Loops/Exercise/06. Strong number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _06._Strong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int originalNumber = number;
+            int factorialSum = 0;
+
+            while (number != 0)
+            {
+                int factorialNumber = 1;
+                int currentDigit = number % 10;
+                number /= 10;
+
+                for (int i = 2; i <= currentDigit; i++)
+                {
+                    factorialNumber *= i;
+                }
+
+                factorialSum += factorialNumber;
+            }
+
+            Console.WriteLine(originalNumber == factorialSum ? "yes" : "no");
+        }
+    }
+}
