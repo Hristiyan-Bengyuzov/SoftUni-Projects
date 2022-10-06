6f52a8ed83a93966b80610261206752defeb1212
Hris <xrisko06@abv.bg>
2022-10-06 21:09:06 +0300

    added project

---
+++ C# Development/C# Fundamentals/Basic Syntax, Conditional Statements and Loops/Exercise/01. Ages/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01._Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine(age switch
+            {
+                <= 2 => "baby",
+                <= 13 => "child",
+                <= 19 => "teenager",
+                <= 65 => "adult",
+                _ => "elder"
+            });
+        }
+    }
+}
