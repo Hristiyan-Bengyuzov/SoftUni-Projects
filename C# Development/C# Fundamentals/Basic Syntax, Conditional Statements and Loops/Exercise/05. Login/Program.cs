9707a73e796eda80922b41adbab15e59c69d1c90
Hris <xrisko06@abv.bg>
2022-10-06 21:09:39 +0300

    added project

---
+++ C# Development/C# Fundamentals/Basic Syntax, Conditional Statements and Loops/Exercise/05. Login/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = new string(username.Reverse().ToArray());
+            int counter = 0;
+            string input = Console.ReadLine();
+
+            while (input != password)
+            {
+                counter++;
+                if (counter == 4)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    return;
+                }
+
+                Console.WriteLine("Incorrect password. Try again.");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"User {username} logged in.");
+        }
+    }
+}
