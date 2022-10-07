3a1b92980812d92542e537d6ea84472fe0041632
Hris <xrisko06@abv.bg>
2022-10-07 23:37:59 +0300

    added project

---
+++ C# Development/C# Fundamentals/Data Types and Variables/Lab/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01._Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+            double kilometers = meters / 1000.0;
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
