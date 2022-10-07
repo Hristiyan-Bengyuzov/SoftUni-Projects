e48ce5bdec131682ccce78239e29e166bc6b7265
Hris <xrisko06@abv.bg>
2022-10-07 23:39:14 +0300

    added project

---
+++ C# Development/C# Fundamentals/Data Types and Variables/Lab/10. Lower or Upper/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _10._Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = char.Parse(Console.ReadLine());
+            Console.WriteLine(char.IsUpper(ch) ? "upper-case" : "lower-case");
+        }
+    }
+}
