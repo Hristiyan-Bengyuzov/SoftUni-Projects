71fa0388840d44cdf88c5c2f9f08546912eabbba
Hris <xrisko06@abv.bg>
2022-10-08 22:45:31 +0300

    added project

---
+++ C# Development/C# Fundamentals/Data Types and Variables/Exercise/02. Sum Digits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02._Sum_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int finalSum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                finalSum += int.Parse(input[i].ToString());
+            }
+
+            Console.WriteLine(finalSum);
+        }
+    }
+}
