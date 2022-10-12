c5b6f9345a1047e305eff3d17c3afa1521548e17
Hris <xrisko06@abv.bg>
2022-10-12 22:27:33 +0300

    added project

---
+++ C# Development/C# Fundamentals/Arrays/Exercise/08. Magic Sum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNum = numbers[i];
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (currentNum + numbers[j] == sum)
+                    {
+                        Console.WriteLine($"{currentNum} {numbers[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
