12ced8813f994cabd6d8877893c36ec1e8e2160c
Hris <xrisko06@abv.bg>
2022-10-12 22:26:49 +0300

    added project

---
+++ C# Development/C# Fundamentals/Arrays/Exercise/02. Common Elements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] arrOne = Console.ReadLine().Split();
+            string[] arrTwo = Console.ReadLine().Split();
+
+            foreach (string currElement in arrOne)
+            {
+                foreach (string secondCurrElement in arrTwo)
+                {
+                    if (currElement == secondCurrElement)
+                    {
+                        Console.Write($"{currElement} ");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
