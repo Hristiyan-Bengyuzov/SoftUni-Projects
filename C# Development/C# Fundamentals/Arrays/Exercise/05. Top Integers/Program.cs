e44b5e76a118b0561a208aae97187c0612983ce8
Hris <xrisko06@abv.bg>
2022-10-12 22:27:12 +0300

    added project

---
+++ C# Development/C# Fundamentals/Arrays/Exercise/05. Top Integers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                int currentIterationNum = inputArray[i];
+                bool currIterattionNumIsBigger = true;
+                for (int j = i + 1; j < inputArray.Length; j++)
+                {
+                    int nextIterationNum = inputArray[j];
+                    if (currentIterationNum <= nextIterationNum)
+                    {
+                        currIterattionNumIsBigger = false;
+                        break;
+                    }
+                }
+
+                if (currIterattionNumIsBigger)
+                {
+                    Console.Write($"{currentIterationNum} ");
+                }
+            }
+        }
+    }
+}
