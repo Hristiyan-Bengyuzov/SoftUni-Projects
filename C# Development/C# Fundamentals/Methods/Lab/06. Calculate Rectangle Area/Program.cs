6006698096352eefe6c434e0139e3eddf4edfe5a
Hris <xrisko06@abv.bg>
2022-10-14 22:24:06 +0300

    added project

---
+++ C# Development/C# Fundamentals/Methods/Lab/06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _06._Calculate_Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double area = CalculateRectangleArea(width, height);
+            Console.WriteLine(area);
+        }
+
+        static double CalculateRectangleArea(double width, double height) => width * height;
+    }
+}
