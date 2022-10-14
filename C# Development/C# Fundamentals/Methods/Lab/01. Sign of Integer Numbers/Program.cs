468fab5d72b2f3105db57ff0e224958587134ac2
Hris <xrisko06@abv.bg>
2022-10-14 22:23:25 +0300

    added project

---
+++ C# Development/C# Fundamentals/Methods/Lab/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01._Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Sign(number);
+        }
+
+        static void Sign(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive. ");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine($"The number {number} is zero. ");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is negative. ");
+            }
+        }
+    }
+}
