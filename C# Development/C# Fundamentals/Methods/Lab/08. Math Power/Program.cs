85a17c486b9e4a72c3d5aa78822efa27e3eb7ab6
Hris <xrisko06@abv.bg>
2022-10-14 22:24:21 +0300

    added project

---
+++ C# Development/C# Fundamentals/Methods/Lab/08. Math Power/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double @base = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+            double result = CalculatePower(@base, power);
+            Console.WriteLine(result);
+        }
+
+        static double CalculatePower(double @base, int power)
+        {
+            double result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= @base;
+            }
+
+            return result;
+        }
+    }
+}
