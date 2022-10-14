7ab9dd5ded747d0be9253582f90c35f666b9c330
Hris <xrisko06@abv.bg>
2022-10-14 22:23:33 +0300

    added project

---
+++ C# Development/C# Fundamentals/Methods/Lab/02. Grades/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02._Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            PrintGradeDefinition(grade);
+        }
+
+        static void PrintGradeDefinition(double grade) => Console.WriteLine(grade >= 2.00 && grade < 3 ? "Fail" : grade < 3.50 ? "Poor" : grade < 4.50 ? "Good" : grade < 5.50 ? "Very good" : "Excellent");
+    }
+}
