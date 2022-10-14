df2048608fdfd4f76798af35a34cb23321a598a9
Hris <xrisko06@abv.bg>
2022-10-14 22:24:43 +0300

    added project

---
+++ C# Development/C# Fundamentals/Methods/Lab/11. Math operations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            double result = Calculate(firstNumber, @operator, secondNumber);
+            Console.WriteLine(result);
+        }
+
+        static double Calculate(double firstNumber, string @operator, double secondNumber)
+        {
+            double result = 0;
+
+            switch (@operator)
+            {
+                case "/":
+                    result = firstNumber/secondNumber;
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
