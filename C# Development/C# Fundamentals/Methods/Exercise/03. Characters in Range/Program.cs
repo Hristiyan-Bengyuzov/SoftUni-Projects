fada2ccf399c5404c93e5fd63235b9a875bdc64b
Hris <xrisko06@abv.bg>
2022-10-16 23:16:04 +0300

    added project

---
+++ C# Development/C# Fundamentals/Methods/Exercise/03. Characters in Range/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            CharactersInRange(firstChar, secondChar);
+        }
+
+        static void CharactersInRange(char firstChar, char secondChar)
+        {
+            if (secondChar < firstChar)
+            {
+                //swap via deconstruction
+                (secondChar, firstChar) = (firstChar, secondChar);
+            }
+
+            for (int i = firstChar + 1; i < secondChar; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}
