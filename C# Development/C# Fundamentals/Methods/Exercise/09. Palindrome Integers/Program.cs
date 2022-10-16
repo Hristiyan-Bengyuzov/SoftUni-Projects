5d320c8d6738f0e70eccb9b7a5f5a0e0e347bb99
Hris <xrisko06@abv.bg>
2022-10-16 23:16:53 +0300

    added project

---
+++ C# Development/C# Fundamentals/Methods/Exercise/09. Palindrome Integers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _09._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                int number = int.Parse(input);
+
+                Console.WriteLine(isPalindrome(number).ToString().ToLower());
+
+                input = Console.ReadLine();
+            }
+        }
+
+        static bool isPalindrome(int n)
+        {
+            int originalValue = n;
+
+            int reversed = 0;
+
+            while (n > 0)
+            {
+                reversed = reversed * 10 + n % 10;
+                n /= 10;
+            }
+
+            return originalValue == reversed;
+        }
+    }
+}
