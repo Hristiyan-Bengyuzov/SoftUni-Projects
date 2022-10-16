1e368cb608473c9dcfd8018d217e88306ab39ba7
Hris <xrisko06@abv.bg>
2022-10-16 23:17:03 +0300

    added project

---
+++ C# Development/C# Fundamentals/Methods/Exercise/10. Top Number/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _10._Top_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (isTopNumber(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static bool isTopNumber(int number)
+        {
+            int sum = 0;
+            int oddDigitCount = 0;
+
+            while (number > 0)
+            {
+                int currentDigit = number % 10;
+
+                if (currentDigit % 2 != 0)
+                {
+                    oddDigitCount++;
+                }
+
+                sum += currentDigit;
+                number /= 10;
+            }
+
+            bool sumDivisibleBy8 = sum % 8 == 0;
+            bool atLeastOneOddDigit = oddDigitCount > 0;
+
+            return sumDivisibleBy8 && atLeastOneOddDigit;
+        }
+    }
+}
