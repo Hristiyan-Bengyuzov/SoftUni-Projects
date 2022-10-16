37590fe1e6c12d8bfc97fd3d24f61edbae472ec5
Hris <xrisko06@abv.bg>
2022-10-16 23:16:21 +0300

    added project

---
+++ C# Development/C# Fundamentals/Methods/Exercise/05. Add and Subtract/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int sum = Sum(firstNum, secondNum);
+            int result = SubtractFromSum(sum, thirdNum);
+
+            Console.WriteLine(result);
+        }
+
+        static int Sum(int firstNum, int secondNum) => firstNum + secondNum;
+
+        static int SubtractFromSum(int sum, int thirdNum) => sum - thirdNum;
+    }
+}
