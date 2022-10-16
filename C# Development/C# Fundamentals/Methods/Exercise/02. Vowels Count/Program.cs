bed2b50a31cd90ce3a13fccd6c06f393607e6042
Hris <xrisko06@abv.bg>
2022-10-16 23:15:55 +0300

    added project

---
+++ C# Development/C# Fundamentals/Methods/Exercise/02. Vowels Count/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToLower();
+
+            int vowelCount = VowelCount(text);
+            Console.WriteLine(vowelCount);
+        }
+
+        static int VowelCount(string text) => text.Count(vowel => "aeiou".Contains(vowel));
+    }
+}
