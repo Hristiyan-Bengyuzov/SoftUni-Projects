4b03f9da30eeb4a8eed6b03fc8d956e945c32afe
Hris <xrisko06@abv.bg>
2022-10-16 23:15:44 +0300

    added project

---
+++ C# Development/C# Fundamentals/Methods/Exercise/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Min(number1, Min(number2, number3)));
+        }
+
+        static int Min(int number1, int number2) => number1 < number2 ? number1 : number2;
+    }
+}
