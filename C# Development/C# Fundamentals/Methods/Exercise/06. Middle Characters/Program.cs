1ed4824d56df677a712d7440bbce0afd52ab2d97
Hris <xrisko06@abv.bg>
2022-10-16 23:16:28 +0300

    added project

---
+++ C# Development/C# Fundamentals/Methods/Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            PrintMiddleCharacters(input);
+        }
+
+        static void PrintMiddleCharacters(string input)
+        {
+            if (input.Length % 2 == 0)
+            {
+                Console.Write(input[input.Length / 2 - 1]);
+                Console.WriteLine(input[input.Length / 2]);
+            }
+            else
+            {
+                Console.WriteLine(input[input.Length / 2]);
+            }
+        }
+    }
+}
