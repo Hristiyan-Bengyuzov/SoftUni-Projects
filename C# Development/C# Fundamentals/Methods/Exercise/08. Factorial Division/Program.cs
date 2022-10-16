cf1b35f0e3888a0336b67459226b5d60003819a4
Hris <xrisko06@abv.bg>
2022-10-16 23:16:43 +0300

    added project

---
+++ C# Development/C# Fundamentals/Methods/Exercise/08. Factorial Division/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            double firstFactorial = Factorial(firstNum);
+            double secondFactorial = Factorial(secondNum);
+            double result = firstFactorial / secondFactorial;
+
+            Console.WriteLine($"{result:f2}");
+        }
+
+        static double Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
