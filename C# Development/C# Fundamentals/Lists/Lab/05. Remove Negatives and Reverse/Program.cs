7e8390062074b6984c56e4a4fe816e1571ba69b0
Hris <xrisko06@abv.bg>
2022-10-18 23:22:58 +0300

    added project

---
+++ C# Development/C# Fundamentals/Lists/Lab/05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Remove_Negatives_and_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> reversedListWithoutNegatives = Console.ReadLine().Split().Select(int.Parse).ToList();
+            reversedListWithoutNegatives.RemoveAll(n => n < 0);
+            reversedListWithoutNegatives.Reverse();
+
+            if (!reversedListWithoutNegatives.Any())
+            {
+                Console.WriteLine("empty");
+                return;
+            }
+
+            Console.WriteLine(string.Join(" ", reversedListWithoutNegatives));
+        }
+    }
+}
