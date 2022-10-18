a2cc8ef13a4ed853bc24801cde8cd18098b6637d
Hris <xrisko06@abv.bg>
2022-10-18 23:22:51 +0300

    added project

---
+++ C# Development/C# Fundamentals/Lists/Lab/04. List of Products/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._List_of_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> products = new List<string>();
+            int productCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < productCount; i++)
+            {
+                string currentProduct = Console.ReadLine();
+                products.Add(currentProduct);
+            }
+
+            products.Sort();
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
