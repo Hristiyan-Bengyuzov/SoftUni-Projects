6ce82f07fffe85f69c45bc1e4020c4f0f9c78b76
Hris <xrisko06@abv.bg>
2022-10-18 23:23:05 +0300

    added project

---
+++ C# Development/C# Fundamentals/Lists/Lab/06. List Manipulation Basics/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._List_Manipulation_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string[] commands = Console.ReadLine().Split();
+
+            while (commands[0] != "end")
+            {
+                string command = commands[0];
+
+                switch (command)
+                {
+
+                    case "Add":
+                        int numberToAdd = int.Parse(commands[1]);
+                        numbers.Add(numberToAdd);
+                        break;
+                    case "Remove":
+                        int numberToRemove = int.Parse(commands[1]);
+                        numbers.Remove(numberToRemove);
+                        break;
+                    case "RemoveAt":
+                        int indexToRemove = int.Parse(commands[1]);
+                        numbers.RemoveAt(indexToRemove);
+                        break;
+                    case "Insert":
+                        int numberToInsert = int.Parse(commands[1]);
+                        int indexToInsert = int.Parse(commands[2]);
+                        numbers.Insert(indexToInsert, numberToInsert);
+                        break;
+                }
+
+                commands = Console.ReadLine().Split();
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
