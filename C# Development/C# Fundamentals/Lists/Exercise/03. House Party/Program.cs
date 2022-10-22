49c33b7c8ea6bb8777045623bb6da37a4490868b
Hris <xrisko06@abv.bg>
2022-10-22 19:56:00 +0300

    added project

---
+++ C# Development/C# Fundamentals/Lists/Exercise/03. House Party/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._House_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            List<string> people = new List<string>();
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] currentPersonInfo = Console.ReadLine().Split();
+                string person = currentPersonInfo[0];
+
+                if (currentPersonInfo[2] == "going!") //going
+                {
+                    if (!people.Contains(person))
+                    {
+                        people.Add(person);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{person} is already in the list!");
+                    }
+
+                }
+                else //not going
+                {
+                    if (people.Contains(person))
+                    {
+                        people.Remove(person);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{person} is not in the list!");
+                    }
+
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine,people));
+        }
+    }
+}
