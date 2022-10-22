ebb2daeb14d8712e2bfdfbfd1ff1375d78ad7a83
Hris <xrisko06@abv.bg>
2022-10-22 19:55:51 +0300

    added project

---
+++ C# Development/C# Fundamentals/Lists/Exercise/02. Change List/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Change_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string[] commands = Console.ReadLine().Split();
+
+            while (commands[0] != "end")
+            {
+                string action = commands[0];
+                int element = int.Parse(commands[1]);
+
+                switch (action)
+                {
+                    case "Delete":
+                        numbers.RemoveAll(number => number == element);
+                        break;
+                    case "Insert":
+                        int position = int.Parse(commands[2]);
+                        numbers.Insert(position, element);
+                        break;
+                }
+
+                commands = Console.ReadLine().Split();
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
