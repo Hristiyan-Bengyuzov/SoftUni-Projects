8b1371ce4a29dfb53adede073fc46e5c0825dd25
Hris <xrisko06@abv.bg>
2022-10-25 23:24:29 +0300

    added project

---
+++ C# Development/C# Fundamentals/Objects and Classes/03. Songs/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Songs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int songsCount = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>();
+
+            for (int i = 0; i < songsCount; i++)
+            {
+                string[] tokens = Console.ReadLine().Split('_');
+                Song song = new Song(tokens[0], tokens[1], tokens[2]);
+                songs.Add(song);
+            }
+
+            string typeList = Console.ReadLine();
+
+            if (typeList=="all")
+            {
+                foreach (Song song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (Song song in songs)
+                {
+                    if (song.TypeList==typeList)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
+        }
+    }
+
+    class Song
+    {
+        public Song(string typeList, string name, string time)
+        {
+            this.TypeList = typeList;
+            this.Name = name;
+            this.Time = time;
+        }
+        public string TypeList { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+    }
+}
