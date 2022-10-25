aae7880162f5d6d0c04843c61beaa7ad377bfb59
Hris <xrisko06@abv.bg>
2022-10-25 23:24:12 +0300

    added project

---
+++ C# Development/C# Fundamentals/Objects and Classes/01. Randomize Words/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ObjectsAndClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+            Random random = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int randomWord = random.Next(words.Length);
+                string currWord = words[i];
+                words[i] = words[randomWord];
+                words[randomWord] = currWord;
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
