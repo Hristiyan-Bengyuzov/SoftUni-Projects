98f4e48564cd37b7595e524fd96df549fbf72719
Hris <xrisko06@abv.bg>
2022-10-30 20:38:05 +0200

    added project

---
+++ C# Development/C# Fundamentals/Objects and Classes/Exercise/04. Students/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentCount = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                Student student = new Student(tokens[0], tokens[1], float.Parse(tokens[2]));
+                students.Add(student);
+            }
+
+            foreach (Student student in students.OrderByDescending(student=>student.Grade))
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+
+    class Student
+    {
+        public Student(string firstName, string lastName, float grade)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Grade = grade;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public float Grade { get; set; }
+
+        public override string ToString() => $"{this.FirstName} {this.LastName}: {this.Grade:f2}";
+
+    }
+}
