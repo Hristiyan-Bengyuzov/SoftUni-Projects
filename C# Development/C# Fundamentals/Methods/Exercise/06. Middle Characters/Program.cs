1a1864b9ce72af92945479520622c3d8be6a842c
Hris <xrisko06@abv.bg>
2022-11-13 00:25:36 +0200

    removed redundant else

--- C# Development/C# Fundamentals/Methods/Exercise/06. Middle Characters/Program.cs
+++ C# Development/C# Fundamentals/Methods/Exercise/06. Middle Characters/Program.cs
@@ -16,12 +16,9 @@ namespace _06._Middle_Characters
             if (input.Length % 2 == 0)
             {
                 Console.Write(input[input.Length / 2 - 1]);
-                Console.WriteLine(input[input.Length / 2]);
             }
-            else
-            {
-                Console.WriteLine(input[input.Length / 2]);
-            }
+
+            Console.WriteLine(input[input.Length / 2]);
         }
     }
 }
