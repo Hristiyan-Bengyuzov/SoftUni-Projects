4e8e717c1b7828a397aed24331b1942e14ba257f
Hris <xrisko06@abv.bg>
2022-11-13 00:43:55 +0200

    added project

---
+++ C# Development/C# Fundamentals/Associative Arrays/Exercise/01. Count Chars in a String/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionaries_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> charByOccurences = new Dictionary<char, int>();
+
+            string[] words = Console.ReadLine().Split();
+
+            foreach (var word in words)
+            {
+                foreach (var character in word)
+                {
+                    if (!charByOccurences.ContainsKey(character))
+                    {
+                        charByOccurences.Add(character, 0);
+                    }
+
+                    charByOccurences[character]++;
+                }
+            }
+
+            foreach (var item in charByOccurences)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
