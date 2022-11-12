5c70574477918b5bedd757ae43e5b41adad3926e
Hris <xrisko06@abv.bg>
2022-11-13 00:44:20 +0200

    added project

---
+++ C# Development/C# Fundamentals/Associative Arrays/Exercise/04. SoftUni Parking/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._SoftUni_Parking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfCommands = int.Parse(Console.ReadLine());
+
+            Dictionary<string, string> peopleByPlateNumber = new Dictionary<string, string>();
+
+            for (int i = 0; i < countOfCommands; i++)
+            {
+                string[] commands = Console.ReadLine().Split();
+                string person = commands[1];
+
+                if (commands[0] == "register")
+                {
+                    string plateNumber = commands[2];
+
+                    if (peopleByPlateNumber.ContainsKey(person))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {plateNumber}");
+                    }
+                    else
+                    {
+                        peopleByPlateNumber.Add(person, plateNumber);
+                        Console.WriteLine($"{person} registered {plateNumber} successfully");
+                    }
+                }
+
+                else if (commands[0] == "unregister")
+                {
+                    if (!peopleByPlateNumber.ContainsKey(person))
+                    {
+                        Console.WriteLine($"ERROR: user {person} not found");
+                    }
+                    else
+                    {
+                        peopleByPlateNumber.Remove(person);
+                        Console.WriteLine($"{person} unregistered successfully");
+                    }
+                }
+
+            }
+
+            foreach (var personByPlateNumber in peopleByPlateNumber)
+            {
+                Console.WriteLine($"{personByPlateNumber.Key} => {personByPlateNumber.Value}");
+            }
+        }
+    }
+}
