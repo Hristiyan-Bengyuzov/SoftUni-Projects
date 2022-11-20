02645582c13163a7a32015508d12173777572361
Hris <xrisko06@abv.bg>
2022-11-20 21:06:19 +0200

    added project

---
+++ C# Development/C# Fundamentals/Text Processing/Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            while (word != "end")
+            {
+                string reversed = string.Empty;
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    reversed += word[i];
+                }
+
+                Console.WriteLine($"{word} = {reversed}");
+
+                word = Console.ReadLine();
+            }
+        }
+    }
+}
