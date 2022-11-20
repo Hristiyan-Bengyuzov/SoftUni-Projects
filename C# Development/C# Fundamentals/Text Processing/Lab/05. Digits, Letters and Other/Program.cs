2f9637fc8af30472745a2f3bb74de763dfc2bf88
Hris <xrisko06@abv.bg>
2022-11-20 21:07:23 +0200

    added project

---
+++ C# Development/C# Fundamentals/Text Processing/Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder digits = new StringBuilder();
+            StringBuilder letters = new StringBuilder();
+            StringBuilder other = new StringBuilder();
+
+            foreach (var character in input)
+            {
+                if (char.IsDigit(character))
+                {
+                    digits.Append(character);
+                }
+                else if (char.IsLetter(character))
+                {
+                    letters.Append(character);
+                }
+                else
+                {
+                    other.Append(character);
+                }
+            }
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(other);
+        }
+    }
+}
