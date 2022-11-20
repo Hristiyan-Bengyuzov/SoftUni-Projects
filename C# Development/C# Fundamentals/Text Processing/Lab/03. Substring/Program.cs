1a41a946737ccdcd3222312331804fc6dc5e4141
Hris <xrisko06@abv.bg>
2022-11-20 21:06:40 +0200

    added project

---
+++ C# Development/C# Fundamentals/Text Processing/Lab/03. Substring/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToRemove = Console.ReadLine();
+            string initialString = Console.ReadLine();
+
+            while (initialString.Contains(stringToRemove))
+            {
+                int index = initialString.IndexOf(stringToRemove);
+                initialString = initialString.Remove(index, stringToRemove.Length);
+            }
+
+            Console.WriteLine(initialString);
+        }
+    }
+}
