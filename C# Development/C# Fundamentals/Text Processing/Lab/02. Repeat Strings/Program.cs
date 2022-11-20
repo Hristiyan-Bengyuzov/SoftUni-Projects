8f911399368a0cca79f4aa23ed6beba56079f64f
Hris <xrisko06@abv.bg>
2022-11-20 21:06:31 +0200

    added project

---
+++ C# Development/C# Fundamentals/Text Processing/Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            foreach (var word in words)
+            {
+                foreach (var character in word)
+                {
+                    Console.Write(word);
+                }
+            }
+
+        }
+    }
+}
