e0b53d096ffe9a594be923133da8555be4640ded
Hris <xrisko06@abv.bg>
2022-11-20 21:07:06 +0200

    added project

---
+++ C# Development/C# Fundamentals/Text Processing/Lab/03. Substring_SecondWay/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Substring_SecondWay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToRemove = Console.ReadLine();
+            string initialString = Console.ReadLine();
+
+            while (initialString.Contains(stringToRemove))
+            {
+                initialString = initialString.Replace(stringToRemove, string.Empty);
+            }
+
+            Console.WriteLine(initialString);
+        }
+    }
+}
