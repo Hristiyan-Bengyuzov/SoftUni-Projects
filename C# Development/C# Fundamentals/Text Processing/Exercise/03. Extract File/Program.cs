45e9e26179c228000dfdc56e6204bdbfa3eeb9b0
Hris <xrisko06@abv.bg>
2022-11-22 22:55:31 +0200

    added project

---
+++ C# Development/C# Fundamentals/Text Processing/Exercise/03. Extract File/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] path = Console.ReadLine().Split('\\');
+
+            string[] file = path[path.Length - 1].Split('.');
+
+            string fileName = file[0];
+            string fileExtension = file[1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+        }
+    }
+}
