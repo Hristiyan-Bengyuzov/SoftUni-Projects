c4914f766e5b24cc0879366bb6719ad6f7e53ee2
Hris <xrisko06@abv.bg>
2022-11-22 22:55:05 +0200

    added project

---
+++ C# Development/C# Fundamentals/Text Processing/Exercise/01. Valid Usernames/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ").ToArray();
+
+            foreach (var username in usernames)
+            {
+                bool isValid = true;
+                if (username.Length >= 3 && username.Length <= 16)
+                {
+                    foreach (var character in username)
+                    {
+                        if (!(character == '_' || character == '-' || char.IsLetterOrDigit(character)))
+                        {
+                            isValid = false;
+                            break;
+                        }
+                    }
+
+                    if (isValid)
+                    {
+                        Console.WriteLine(username);
+                    }
+                }
+            }
+
+        }
+    }
+}
