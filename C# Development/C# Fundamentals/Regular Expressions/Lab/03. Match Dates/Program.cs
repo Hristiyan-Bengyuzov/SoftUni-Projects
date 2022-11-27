b80586ffe5f773d3e1f90b6b7c7bd5b5dfc9ef48
Hris <xrisko06@abv.bg>
2022-11-27 22:25:25 +0200

    added project

---
+++ C# Development/C# Fundamentals/Regular Expressions/Lab/03. Match Dates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(?<day>\d{2})(?<separator>[\.\-\/])(?<month>[A-Z][a-z]{2})\k<separator>(?<year>\d{4})\b";
+
+            Regex regex = new Regex(pattern);
+            MatchCollection validDates = regex.Matches(input);
+
+            foreach (Match date in validDates)
+            {
+                Console.WriteLine($"Day: {date.Groups["day"]}, Month: {date.Groups["month"]}, Year: {date.Groups["year"]}");
+            }
+
+        }
+    }
+}
