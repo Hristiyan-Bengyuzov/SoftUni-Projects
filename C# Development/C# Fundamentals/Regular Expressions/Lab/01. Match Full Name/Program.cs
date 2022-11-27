96e18ff515f3ba5e99ca4f4a03958d7b2c74f6c3
Hris <xrisko06@abv.bg>
2022-11-27 22:25:07 +0200

    added project

---
+++ C# Development/C# Fundamentals/Regular Expressions/Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string names = Console.ReadLine();
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(names);
+
+            Console.WriteLine(string.Join(" ", matches));
+        }
+    }
+}
