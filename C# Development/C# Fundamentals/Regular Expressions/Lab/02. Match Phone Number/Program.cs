eb58fa77edc8cae4c49e75ff95d307ba6702f3ba
Hris <xrisko06@abv.bg>
2022-11-27 22:25:17 +0200

    added project

---
+++ C# Development/C# Fundamentals/Regular Expressions/Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phoneNumbers = Console.ReadLine();
+            string pattern = @"\+359(?<separator>[ \-])2\k<separator>\d{3}\k<separator>\d{4}\b";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(phoneNumbers);
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
