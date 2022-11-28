74e2756894a2c72dbc30035a1daddcd43c6bf859
Hris <xrisko06@abv.bg>
2022-11-28 22:44:07 +0200

    added project

---
+++ C# Development/C# Fundamentals/Regular Expressions/Exercise/06. Extract Emails/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06._Extract_Emails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex regex = new Regex(@"(^|\s)[0-9A-Za-z][\w*\-\.]*[0-9A-Za-z]@[A-Za-z]+([.-][A-Za-z]+)+\b");
+            MatchCollection matches = regex.Matches(input);
+            Console.WriteLine(string.Join(Environment.NewLine, matches));
+        }
+    }
+}
