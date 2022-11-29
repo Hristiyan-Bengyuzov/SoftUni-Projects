37a3c9dbac3a939f0308194e70ed67454391f218
Hris <xrisko06@abv.bg>
2022-11-29 22:27:11 +0200

    added project

---
+++ C# Development/C# Advanced/Stacks and Queues/Lab/4. Matching Brackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var symbol = input[i];
+
+                if (symbol == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (symbol == ')')
+                {
+                    int startIndex = stack.Pop();
+                    string result = input.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
