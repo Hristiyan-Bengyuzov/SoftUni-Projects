39690c3c1495b2e6f2c253b1d8543466310cf3fc
Hris <xrisko06@abv.bg>
2022-11-29 22:27:23 +0200

    added project

---
+++ C# Development/C# Advanced/Stacks and Queues/Lab/5. Print Even Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var queue = new Queue<int>();
+
+            // LINQ alternative:
+            // var queue = new Queue<int>(input.Where(x => x % 2 == 0));
+
+            foreach (var item in input)
+            {
+                if (item % 2 == 0)
+                {
+                    queue.Enqueue(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", queue));
+        }
+    }
+}
