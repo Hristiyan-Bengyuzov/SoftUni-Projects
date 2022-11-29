b535be38b3c5eb9fb61a3b0030464a29da82a910
Hris <xrisko06@abv.bg>
2022-11-29 22:26:35 +0200

    added project

---
+++ C# Development/C# Advanced/Stacks and Queues/Lab/1. Reverse a String/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Reverse_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(string.Empty, new Stack<char>(Console.ReadLine())));
+        }
+    }
+}
