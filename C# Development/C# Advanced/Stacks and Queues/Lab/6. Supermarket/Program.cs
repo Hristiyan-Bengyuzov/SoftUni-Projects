c8245195ae4374c44f9e984f51d97415e48debb2
Hris <xrisko06@abv.bg>
2022-11-29 22:27:34 +0200

    added project

---
+++ C# Development/C# Advanced/Stacks and Queues/Lab/6. Supermarket/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customers = new Queue<string>();
+
+            while (true)
+            {
+                var name = Console.ReadLine();
+
+                if (name == "End")
+                {
+                    break;
+                }
+
+                if (name == "Paid")
+                {
+                    while (customers.Any())
+                    {
+                        Console.WriteLine(customers.Dequeue());
+                    }
+                }
+                else
+                {
+                    customers.Enqueue(name);
+                }
+
+            }
+
+            Console.WriteLine($"{customers.Count} people remaining.");
+        }
+    }
+}
