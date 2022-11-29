1e47984e97d5b2310645df50f7aacf031ca793d5
Hris <xrisko06@abv.bg>
2022-11-29 22:27:43 +0200

    added project

---
+++ C# Development/C# Advanced/Stacks and Queues/Lab/7. Hot Potato/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var players = new Queue<string>(Console.ReadLine().Split());
+            int toss = int.Parse(Console.ReadLine());
+
+            while (players.Count > 1)
+            {
+                for (int i = 1; i < toss; i++)
+                {
+                    players.Enqueue(players.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {players.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {players.Dequeue()}");
+        }
+    }
+}
