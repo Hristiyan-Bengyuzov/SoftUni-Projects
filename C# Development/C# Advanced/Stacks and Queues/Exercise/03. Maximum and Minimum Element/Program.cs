34c31fb7775645969141ee80c28fc3a641d7f053
Hris <xrisko06@abv.bg>
2022-12-06 23:37:34 +0200

    added project

---
+++ C# Development/C# Advanced/Stacks and Queues/Exercise/03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Maximum_and_Minimum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                var commands = Console.ReadLine().Split();
+
+                switch (commands[0])
+                {
+                    case "1":
+                        stack.Push(int.Parse(commands[1]));
+                        break;
+                    case "2":
+                        if (stack.Any())
+                        {
+                            stack.Pop();
+                        }
+                        break;
+                    case "3":
+                        if (stack.Any())
+                        {
+                            Console.WriteLine(stack.Max());
+                        }
+                        break;
+                    case "4":
+                        if (stack.Any())
+                        {
+                            Console.WriteLine(stack.Min());
+                        }
+                        break;
+                }
+
+            }
+
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
