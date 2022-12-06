8f4d257f78a49668dd40902f0b803dc241283e12
Hris <xrisko06@abv.bg>
2022-12-06 23:37:57 +0200

    added project

---
+++ C# Development/C# Advanced/Stacks and Queues/Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var clothes = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            int capacity = int.Parse(Console.ReadLine());
+
+            int racks = 1;
+            int currentCapacity = capacity;
+
+            while (clothes.Any())
+            {
+                var currentCloth = clothes.Peek();
+
+                if (currentCapacity >= currentCloth)
+                {
+                    currentCapacity -= currentCloth;
+                    clothes.Pop();
+                }
+                else
+                {
+                    racks++;
+                    currentCapacity = capacity;
+                }
+            }
+
+            Console.WriteLine(racks);
+        }
+    }
+}
