13370aaeb4f2db14ea0ba1b7db14f7b25a49e312
Hris <xrisko06@abv.bg>
2022-12-06 23:37:46 +0200

    added project

---
+++ C# Development/C# Advanced/Stacks and Queues/Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+            var orders = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            Console.WriteLine(orders.Max());
+
+            while (orders.Any())
+            {
+                int currentOrder = orders.Peek();
+
+                if (foodQuantity >= currentOrder)
+                {
+                    foodQuantity -= currentOrder;
+                    orders.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
