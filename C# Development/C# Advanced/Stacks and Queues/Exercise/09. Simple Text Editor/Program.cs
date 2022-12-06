0daace4a884c7fe429c21a28c7ddf01d1755e416
Hris <xrisko06@abv.bg>
2022-12-06 23:38:39 +0200

    added project

---
+++ C# Development/C# Advanced/Stacks and Queues/Exercise/09. Simple Text Editor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09._Simple_Text_Editor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            var changes = new Stack<string>();
+
+            StringBuilder text = new StringBuilder();
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                var commands = Console.ReadLine().Split();
+
+                switch (commands[0])
+                {
+                    case "1":
+                        changes.Push(text.ToString());
+                        text.Append(commands[1]);
+                        break;
+                    case "2":
+                        int countToRemove = int.Parse(commands[1]);
+                        changes.Push(text.ToString());
+                        text.Remove(text.Length - countToRemove, countToRemove);
+                        break;
+                    case "3":
+                        int positionToReturn = int.Parse(commands[1]);
+                        Console.WriteLine(text[positionToReturn - 1]);
+                        break;
+                    case "4":
+                        string latestChange = changes.Pop();
+                        text.Clear();
+                        text.Append(latestChange);
+                        break;
+                }
+            }
+        }
+    }
+}
