b2be6c31eaded3e7587e83c521e388a75e436946
Hris <xrisko06@abv.bg>
2022-12-06 23:38:07 +0200

    added project

---
+++ C# Development/C# Advanced/Stacks and Queues/Exercise/06. Songs Queue/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Songs_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var songs = new Queue<string>(Console.ReadLine().Split(", "));
+
+            while (songs.Any())
+            {
+                var commands = Console.ReadLine().Split();
+
+                switch (commands[0])
+                {
+                    case "Add":
+                        string songToAdd = string.Join(" ", commands.Skip(1));
+                        if (!songs.Contains(songToAdd))
+                        {
+                            songs.Enqueue(songToAdd);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{songToAdd} is already contained!");
+                        }
+                        break;
+                    case "Play":
+                        songs.Dequeue();
+                        break;
+                    case "Show":
+                        Console.WriteLine(string.Join(", ", songs));
+                        break;
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
