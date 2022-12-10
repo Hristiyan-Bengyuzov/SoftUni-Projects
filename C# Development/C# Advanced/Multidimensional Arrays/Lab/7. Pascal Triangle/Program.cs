9520f9498f0e89c67a0dd5dcf190a1916311a1ff
Hris <xrisko06@abv.bg>
2022-12-10 23:42:45 +0200

    added project

---
+++ C# Development/C# Advanced/Multidimensional Arrays/Lab/7. Pascal Triangle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _7._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            long[][] pascalTriangle = new long[rows][];
+
+            pascalTriangle[0] = new long[1] { 1 };
+
+            for (int row = 1; row < rows; row++)
+            {
+                pascalTriangle[row] = new long[row + 1];
+                pascalTriangle[row][0] = 1;
+
+                for (int col = 1; col < row; col++)
+                {
+                    pascalTriangle[row][col] = pascalTriangle[row - 1][col] + pascalTriangle[row - 1][col - 1];
+                }
+
+                pascalTriangle[row][row] = 1;
+            }
+
+            for (int row = 0; row < pascalTriangle.Length; row++)
+            {
+                for (int col = 0; col < pascalTriangle[row].Length; col++)
+                {
+                    Console.Write(pascalTriangle[row][col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
