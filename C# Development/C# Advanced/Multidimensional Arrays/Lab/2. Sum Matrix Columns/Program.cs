4c4f73ba78bc1845f7f3b1f39618d2639a320599
Hris <xrisko06@abv.bg>
2022-12-10 23:42:04 +0200

    added project

---
+++ C# Development/C# Advanced/Multidimensional Arrays/Lab/2. Sum Matrix Columns/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = ParseArrayFromConsole(',', ' ');
+
+            int[,] matrix = new int[input[0], input[1]];
+
+            InitializeMatrix(matrix);
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int currentColSum = 0;
+
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    currentColSum += matrix[row, col];
+                }
+
+                Console.WriteLine(currentColSum);
+            }
+        }
+
+        static void InitializeMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var colElements = ParseArrayFromConsole(' ');
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+        }
+
+        static int[] ParseArrayFromConsole(params char[] splitSymbols) => Console.ReadLine().Split(splitSymbols, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    }
+}
