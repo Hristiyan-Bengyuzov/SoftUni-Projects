43979ad0143c586b86d9a4d4a2609d354855c65b
Hris <xrisko06@abv.bg>
2022-12-11 23:42:39 +0200

    added project

---
+++ C# Development/C# Advanced/Multidimensional Arrays/Exercise/2. Squares in Matrix/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Squares_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            char[,] matrix = new char[input[0], input[1]];
+
+            InitializeCharMatrix(matrix);
+
+            int equalChar2x2Squares = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] &&
+                        matrix[row, col] == matrix[row + 1, col] &&
+                        matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        equalChar2x2Squares++;
+                    }
+                }
+            }
+
+            Console.WriteLine(equalChar2x2Squares);
+        }
+        static void InitializeCharMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] colElements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+        }
+    }
+}
