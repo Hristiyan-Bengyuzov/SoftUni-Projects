eb369e4e603c032be0959adbc0603edb7d026ea8
Hris <xrisko06@abv.bg>
2022-12-14 23:42:16 +0200

    added project

---
+++ C# Development/C# Advanced/Sets and Dictionaries Advanced/Lab/02. Average Student Grades/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStudents = int.Parse(Console.ReadLine());
+            var studentGrades = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                var currentStudentInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string studentName = currentStudentInfo[0];
+                decimal grade = decimal.Parse(currentStudentInfo[1]);
+
+                if (!studentGrades.ContainsKey(studentName))
+                {
+                    studentGrades.Add(studentName, new List<decimal>());
+                }
+
+                studentGrades[studentName].Add(grade);
+            }
+
+            foreach (var (student, grades) in studentGrades)
+            {
+                Console.Write($"{student} -> ");
+
+                foreach (var grade in grades)
+                {
+                    Console.Write($"{grade:f2} ");
+                }
+
+                Console.WriteLine($"(avg: {grades.Average():f2})");
+            }
+        }
+    }
+}
