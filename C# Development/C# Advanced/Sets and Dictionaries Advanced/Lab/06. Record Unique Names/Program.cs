e9d6f1eecb7e9133b33c1890d099428a001db2c0
Hris <xrisko06@abv.bg>
2022-12-14 23:42:46 +0200

    added project

---
+++ C# Development/C# Advanced/Sets and Dictionaries Advanced/Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfNames = int.Parse(Console.ReadLine());
+            var uniqueNames = new HashSet<string>();
+
+            for (int i = 0; i < countOfNames; i++)
+            {
+                string name = Console.ReadLine();
+                uniqueNames.Add(name);
+            }
+
+            // printing
+            foreach (var uniqueName in uniqueNames)
+            {
+                Console.WriteLine(uniqueName);
+            }
+        }
+    }
+}
