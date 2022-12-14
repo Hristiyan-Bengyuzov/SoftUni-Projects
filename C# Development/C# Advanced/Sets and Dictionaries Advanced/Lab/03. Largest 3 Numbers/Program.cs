39a83ccae9d39b40018e133e6e142348d614c510
Hris <xrisko06@abv.bg>
2022-12-14 23:42:23 +0200

    added project

---
+++ C# Development/C# Advanced/Sets and Dictionaries Advanced/Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            // print 3 largest
+            foreach (var item in arr.OrderByDescending(x => x).Take(3))
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
