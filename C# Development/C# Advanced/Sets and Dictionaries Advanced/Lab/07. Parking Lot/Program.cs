988aeb1fe7f63708d16b8a53fa83a714192030a0
Hris <xrisko06@abv.bg>
2022-12-14 23:42:53 +0200

    added project

---
+++ C# Development/C# Advanced/Sets and Dictionaries Advanced/Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var carNumbers = new HashSet<string>();
+
+            while (true)
+            {
+                var input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (input[0] == "END")
+                {
+                    break;
+                }
+
+                string direction = input[0];
+                string carNumber = input[1];
+
+                if (direction == "IN")
+                {
+                    carNumbers.Add(carNumber);
+                }
+                else
+                {
+                    carNumbers.Remove(carNumber);
+                }
+
+            }
+
+
+            // printing
+            if (!carNumbers.Any())
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var carNumber in carNumbers)
+                {
+                    Console.WriteLine(carNumber);
+                }
+            }
+        }
+    }
+}
