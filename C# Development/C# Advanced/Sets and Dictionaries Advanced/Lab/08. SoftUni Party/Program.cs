f4718205a3fa75569a61d3b2c239ad304bccb903
Hris <xrisko06@abv.bg>
2022-12-14 23:42:59 +0200

    added project

---
+++ C# Development/C# Advanced/Sets and Dictionaries Advanced/Lab/08. SoftUni Party/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._SoftUni_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var members = new HashSet<string>();
+
+            while (true)
+            {
+                string memberToAdd = Console.ReadLine();
+
+                if (memberToAdd == "PARTY")
+                {
+                    break;
+                }
+
+                members.Add(memberToAdd);
+            }
+
+            while (true)
+            {
+                string memberToRemove = Console.ReadLine();
+
+                if (memberToRemove == "END")
+                {
+                    break;
+                }
+
+                members.Remove(memberToRemove);
+            }
+
+            // printing
+            Console.WriteLine(members.Count);
+            foreach (var member in members.OrderByDescending(x => StartsWithDigit(x)))
+            {
+                Console.WriteLine(member);
+            }
+        }
+
+        static bool StartsWithDigit(string input) => char.IsDigit(input[0]);
+
+    }
+}
