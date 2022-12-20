d0892b212cb14cf60c99a4540c23e9107a76114b
Hris <xrisko06@abv.bg>
2022-12-20 23:52:18 +0200

    added alternate solution

---
+++ C# Development/C# Advanced/Sets and Dictionaries Advanced/Exercise/02. Sets of Elements (Intersection with LINQ)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var firstSet = new HashSet<int>();
+            var secondSet = new HashSet<int>();
+           
+            for (int i = 0; i < input[0]; i++)
+            {
+                firstSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < input[1]; i++)
+            {
+                secondSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            var intersection = firstSet.Intersect(secondSet);
+            Console.WriteLine(string.Join(" ", intersection));
+        }
+    }
+}
