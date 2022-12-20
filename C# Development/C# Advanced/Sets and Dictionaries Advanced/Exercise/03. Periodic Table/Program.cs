145707a5587902416d2ef8a8bb34e7572e559501
Hris <xrisko06@abv.bg>
2022-12-20 23:44:42 +0200

    added project

---
+++ C# Development/C# Advanced/Sets and Dictionaries Advanced/Exercise/03. Periodic Table/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfCommands = int.Parse(Console.ReadLine());
+            var uniqueChemicalElements = new HashSet<string>();
+
+            for (int i = 0; i < countOfCommands; i++)
+            {
+                var currentChemicalElements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+                currentChemicalElements.ForEach(e => uniqueChemicalElements.Add(e));
+            }
+
+            // printing
+            uniqueChemicalElements.OrderBy(e => e).ToList().ForEach(e => Console.Write($"{e} "));
+        }
+    }
+}
