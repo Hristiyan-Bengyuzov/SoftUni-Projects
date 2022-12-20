b2bbb0857b7782bf988eb468b6c878bfcf385f49
Hris <xrisko06@abv.bg>
2022-12-20 23:44:58 +0200

    added project

---
+++ C# Development/C# Advanced/Sets and Dictionaries Advanced/Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var charsByOccurences = new Dictionary<char, int>();
+
+            foreach (var character in input)
+            {
+                if (!charsByOccurences.ContainsKey(character))
+                {
+                    charsByOccurences.Add(character, 0);
+                }
+
+                charsByOccurences[character]++;
+            }
+
+            // printing
+            foreach (var (character, occurences) in charsByOccurences.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{character}: {occurences} time/s");
+            }
+        }
+    }
+}
