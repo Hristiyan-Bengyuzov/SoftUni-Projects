48e8259c8d29fb7f37f0e5373eb8b1e517273519
Hris <xrisko06@abv.bg>
2022-12-20 23:44:23 +0200

    added project

---
+++ C# Development/C# Advanced/Sets and Dictionaries Advanced/Exercise/01. Unique Usernames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfUsernames = int.Parse(Console.ReadLine());
+            var uniqueUsernames = new HashSet<string>();
+
+            for (int i = 0; i < countOfUsernames; i++)
+            {
+                uniqueUsernames.Add(Console.ReadLine());
+            }
+
+            // printing
+            uniqueUsernames.ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
