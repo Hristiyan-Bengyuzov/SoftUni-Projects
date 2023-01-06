be1ee1703c763c4d78962673d80fae39152329b2
Hris <xrisko06@abv.bg>
2023-01-06 23:14:38 +0200

    added project

---
+++ C# Development/C# Advanced/Streams, Files and Directories/Lab/03. Word Count/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace WordCount
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            string[] wantedWords;
+
+            using (var reader = new StreamReader(wordsFilePath))
+            {
+                wantedWords = reader.ReadToEnd().Split();
+            } // reading all wanted words
+
+            string[] inputText;
+            char[] separators = { ' ', '.', ',', '-', '?', '!' };
+
+            using (var reader = new StreamReader(textFilePath))
+            {
+                inputText = reader.ReadToEnd().Split(separators);
+            } // reading input
+
+            var wordOccurences = new Dictionary<string, int>();
+
+            foreach (var wantedWord in wantedWords)
+            {
+                foreach (var inputWord in inputText)
+                {
+                    if (wantedWord.ToLower() == inputWord.ToLower())
+                    {
+                        if (!wordOccurences.ContainsKey(wantedWord))
+                        {
+                            wordOccurences.Add(wantedWord, 0);
+                        }
+
+                        wordOccurences[wantedWord]++;
+                    }
+                }
+            } // calculating occurences of each wanted word
+
+            using (var writer = new StreamWriter(outputFilePath))
+            {
+                foreach (var word in wordOccurences.OrderByDescending(x => x.Value))
+                {
+                    writer.WriteLine($"{word.Key} - {word.Value}");
+                }
+            } // writing the result in an output file
+        }
+    }
+}
