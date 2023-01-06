294a0ee686cb59415da877721cbacf344422d86b
Hris <xrisko06@abv.bg>
2023-01-06 23:15:30 +0200

    added project

---
+++ C# Development/C# Advanced/Streams, Files and Directories/Lab/07. Folder Size/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace FolderSize
+{
+    public class FolderSize
+    {
+        static void Main()
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            DirectoryInfo dir = new DirectoryInfo(folderPath);
+            FileInfo[] files = dir.GetFiles("*", SearchOption.AllDirectories);
+
+            // size in bytes
+            double sum = files.Sum(f => f.Length);
+
+            using (var writer = new StreamWriter(outputFilePath))
+            {
+                writer.Write(sum / 1024);
+            }
+        }
+    }
+}
