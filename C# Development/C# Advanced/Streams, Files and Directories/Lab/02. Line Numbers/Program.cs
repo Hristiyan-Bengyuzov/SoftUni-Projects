5be37d570ad3b798c59469ab58809a055c0b5a6e
Hris <xrisko06@abv.bg>
2023-01-06 23:14:23 +0200

    added project

---
+++ C# Development/C# Advanced/Streams, Files and Directories/Lab/02. Line Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    int lineNumber = 1;
+
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        writer.WriteLine($"{lineNumber++}. {line}");
+                    }
+                }
+            }
+        }
+    }
+}
