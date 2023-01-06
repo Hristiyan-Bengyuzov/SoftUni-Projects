49931f9c7f8b51ce46e0338c8ccb2ae8071d6d12
Hris <xrisko06@abv.bg>
2023-01-06 23:14:49 +0200

    added project

---
+++ C# Development/C# Advanced/Streams, Files and Directories/Lab/04. Merge Text Files/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace MergeFiles
+{
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            var firstReader = new StreamReader(firstInputFilePath);
+            var secondReader = new StreamReader(secondInputFilePath);
+            var writer = new StreamWriter(outputFilePath);
+
+            Action<StreamReader, StreamWriter> writeLineFromReader = (reader, writer) =>
+            {
+                if (!reader.EndOfStream)
+                {
+                    writer.WriteLine(reader.ReadLine());
+                }
+            };
+
+            while (!firstReader.EndOfStream || !secondReader.EndOfStream)
+            {
+                writeLineFromReader(firstReader, writer);
+                writeLineFromReader(secondReader, writer);
+            }
+
+            writer.Close();
+            firstReader.Close();
+            secondReader.Close();
+        }
+    }
+}
