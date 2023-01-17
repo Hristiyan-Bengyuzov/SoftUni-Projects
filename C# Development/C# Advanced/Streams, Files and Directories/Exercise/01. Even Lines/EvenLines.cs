d246e2ade7b8c4f3d4e5fc3a318255d9b3b75cb5
Hris <xrisko06@abv.bg>
2023-01-17 22:57:23 +0200

    added project

---
+++ C# Development/C# Advanced/Streams, Files and Directories/Exercise/01. Even Lines/EvenLines.cs
@@ -0,0 +1,59 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            var sb = new StringBuilder();
+            using (StreamReader streamReader = new StreamReader(inputFilePath))
+            {
+                string line = string.Empty;
+                int count = 0;
+
+                while (!streamReader.EndOfStream)
+                {
+                    line = streamReader.ReadLine();
+
+                    if (count++ % 2 == 0)
+                    {
+                        string replacedSymbols = ReplaceSymbols(line);
+                        sb.AppendLine(ReverseWords(replacedSymbols));
+                    }
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static string ReplaceSymbols(string line)
+        {
+            char[] symbolsToReplace = { '-', ',', '.', '!', '?' };
+
+            foreach (var symbol in symbolsToReplace)
+            {
+                line = line.Replace(symbol, '@');
+            }
+
+            return line;
+        }
+
+        private static string ReverseWords(string replacedSymbols)
+        {
+            return string.Join(' ', replacedSymbols
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Reverse());
+        }
+    }
+}
