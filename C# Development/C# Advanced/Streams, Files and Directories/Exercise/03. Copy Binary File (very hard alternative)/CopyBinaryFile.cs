4578f55fea1478a1b078da25a7ec6d2d5db0ada2
Hris <xrisko06@abv.bg>
2023-01-17 22:58:01 +0200

    added alternate solution

---
+++ C# Development/C# Advanced/Streams, Files and Directories/Exercise/03. Copy Binary File (very hard alternative)/CopyBinaryFile.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace CopyBinaryFile
+{
+    public class CopyBinaryFile
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\copyMe.png";
+            string outputPath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputPath, outputPath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            File.Copy(inputFilePath, outputFilePath);
+        }
+    }
+}
