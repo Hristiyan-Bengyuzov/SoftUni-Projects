d5592fe8ed007ba92e97d58b168918822cdcfb6a
Hris <xrisko06@abv.bg>
2023-01-23 22:56:16 +0200

    added project

---
+++ C# Development/C# Advanced/Functional Programming/Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03._Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> startsWithUpper = word => char.IsUpper(word[0]);
+
+            Console.WriteLine(string.Join(Environment.NewLine,
+                Array.FindAll(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries), startsWithUpper)));
+        }
+    }
+}
