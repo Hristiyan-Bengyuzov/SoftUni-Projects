6c52543025d829c3da7617d285027deda6abf326
Hris <xrisko06@abv.bg>
2023-01-23 22:56:26 +0200

    added project

---
+++ C# Development/C# Advanced/Functional Programming/Lab/04. Add VAT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, string> addVAT = x => (decimal.Parse(x) * 1.2m).ToString("f2");
+
+            Console.WriteLine(string.Join(Environment.NewLine, Console.ReadLine()
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(addVAT)));
+        }
+    }
+}
