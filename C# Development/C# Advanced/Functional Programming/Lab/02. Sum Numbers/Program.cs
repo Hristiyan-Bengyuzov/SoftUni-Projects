c5668ae1e812d00c39b6dfdabc9df30981eab975
Hris <xrisko06@abv.bg>
2023-01-23 22:56:05 +0200

    added project

---
+++ C# Development/C# Advanced/Functional Programming/Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parser = x => int.Parse(x);
+            var arr = Console.ReadLine().Split(',', StringSplitOptions.RemoveEmptyEntries).Select(parser).ToArray();
+
+            Console.WriteLine(arr.Length);
+            Console.WriteLine(arr.Sum());
+        }
+    }
+}
