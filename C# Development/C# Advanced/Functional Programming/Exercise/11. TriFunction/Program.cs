af6c66199c65222541d9daba41311bdbcdad380d
Hris <xrisko06@abv.bg>
2023-01-26 22:49:48 +0200

    added project

---
+++ C# Development/C# Advanced/Functional Programming/Exercise/11. TriFunction/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _11._TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int, bool> isNameSumEqualOrLarger = (name, threshold) => name.Sum(ch => ch) >= threshold;
+            Func<string[], int, Func<string, int, bool>, string> returnFirstName = (names, threshold, isMatch) => names.FirstOrDefault(name => isMatch(name, threshold));
+
+            int threshold = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+
+            Console.WriteLine(returnFirstName(names, threshold, isNameSumEqualOrLarger));
+        }
+
+    }
+}
