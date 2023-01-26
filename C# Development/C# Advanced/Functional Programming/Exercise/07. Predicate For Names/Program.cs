13d8ab7cd0a0bb2343cfae478e820bfbec19130e
Hris <xrisko06@abv.bg>
2023-01-26 22:48:57 +0200

    added project

---
+++ C# Development/C# Advanced/Functional Programming/Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<List<string>, Predicate<string>, List<string>> removeInvalidNames = (names, filter) =>
+            {
+                names.RemoveAll(filter);
+                return names;
+            };
+
+            Action<List<string>> print = list => list.ForEach(Console.WriteLine);
+
+            int lengthToFilter = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            names = removeInvalidNames(names, name => name.Length > lengthToFilter);
+            print(names);
+        }
+    }
+}
