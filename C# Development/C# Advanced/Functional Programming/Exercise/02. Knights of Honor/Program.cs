75196e3c44883931324aa20d2cdb96178be39b02
Hris <xrisko06@abv.bg>
2023-01-26 22:48:01 +0200

    added project

---
+++ C# Development/C# Advanced/Functional Programming/Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            Action<List<string>> printNames = names => names.ForEach(n => Console.WriteLine($"Sir {n}"));
+            printNames(names);
+        }
+    }
+}
