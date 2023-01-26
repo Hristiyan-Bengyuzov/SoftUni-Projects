8a3725364b4b2b8a6d093f214ba76d81eb621722
Hris <xrisko06@abv.bg>
2023-01-26 22:49:08 +0200

    added project

---
+++ C# Development/C# Advanced/Functional Programming/Exercise/08. List Of Predicates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._List_Of_Predicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<List<int>, Predicate<int>, List<int>> findDivisible = (numbers, isDivisible) => numbers.FindAll(isDivisible);
+
+            int range = int.Parse(Console.ReadLine());
+            List<int> numbers = Enumerable.Range(1, range).ToList();
+            List<Predicate<int>> predicates = new List<Predicate<int>>();
+            List<int> dividers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            foreach (var divider in dividers)
+                predicates.Add(p => p % divider == 0);
+
+            foreach (var predicate in predicates)
+                numbers = findDivisible(numbers, predicate);
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
