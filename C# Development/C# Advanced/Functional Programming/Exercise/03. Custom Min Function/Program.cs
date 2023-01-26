e8c742cb2f5ad67deaf385707e858044a962fb91
Hris <xrisko06@abv.bg>
2023-01-26 22:48:13 +0200

    added project

---
+++ C# Development/C# Advanced/Functional Programming/Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Func<int[], int> min = numbers => numbers.Min();
+            Console.WriteLine(min(numbers));
+        }
+    }
+}
