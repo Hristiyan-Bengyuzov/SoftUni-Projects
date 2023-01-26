0aa9168cdfaee59c3f28e3bf471fad605f01f9e6
Hris <xrisko06@abv.bg>
2023-01-26 22:47:49 +0200

    added project

---
+++ C# Development/C# Advanced/Functional Programming/Exercise/01. Action Print/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            Action<List<string>> printNames = names => names.ForEach(Console.WriteLine);
+            printNames(names);
+        }
+    }
+}
