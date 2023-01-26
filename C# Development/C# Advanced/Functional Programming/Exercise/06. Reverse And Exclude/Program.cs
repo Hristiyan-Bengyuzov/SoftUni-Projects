85f3acb9d0838f2b0f6ca46c33bdf608360e3296
Hris <xrisko06@abv.bg>
2023-01-26 22:48:46 +0200

    added project

---
+++ C# Development/C# Advanced/Functional Programming/Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<List<int>, List<int>> reverse = list =>
+            {
+                List<int> reversedList = new List<int>();
+                for (int i = list.Count - 1; i >= 0; i--)
+                    reversedList.Add(list[i]);
+
+                return reversedList;
+            };
+
+            Func<List<int>, Predicate<int>, List<int>> excludeDivisible = (numbers, isDivisible) =>
+            {
+                numbers.RemoveAll(isDivisible);
+                return numbers;
+            };
+
+            var numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            int divisor = int.Parse(Console.ReadLine());
+
+            numbers = reverse(numbers);
+            numbers = excludeDivisible(numbers, x => x % divisor == 0);
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
