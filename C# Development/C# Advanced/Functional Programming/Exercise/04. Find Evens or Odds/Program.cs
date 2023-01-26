fd6fd68f79a93c853f1df379eb6c4faab32afad9
Hris <xrisko06@abv.bg>
2023-01-26 22:48:26 +0200

    added project

---
+++ C# Development/C# Advanced/Functional Programming/Exercise/04. Find Evens or Odds/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Find_Evens_or_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> isOdd = x => x % 2 != 0;
+            Predicate<int> isEven = x => x % 2 == 0;
+            int[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            List<int> result = new List<int>();
+
+            for (int i = input[0]; i <= input[1]; i++)
+                result.Add(i);
+
+            string oddOrEven = Console.ReadLine();
+            Console.WriteLine(oddOrEven == "odd" ? string.Join(' ', result.FindAll(isOdd))
+                : string.Join(' ', result.FindAll(isEven)));
+        }
+    }
+}
