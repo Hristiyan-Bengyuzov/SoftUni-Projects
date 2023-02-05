7b866449318643acab2e341eb1eb07222e4661cb
Hris <xrisko06@abv.bg>
2023-02-05 21:55:12 +0200

    added project

---
+++ C# Development/C# Advanced/Defining Classes/Lab/04. Car Engine And Tires/Tire.cs
@@ -0,0 +1,26 @@
+﻿namespace CarManufacturer
+{
+    public class Tire
+    {
+        private int year;
+        private double pressure;
+
+        public Tire(int year, double pressure)
+        {
+            this.Year = year;
+            this.Pressure = pressure;
+        }
+
+        public int Year
+        {
+            get => this.year;
+            set => this.year = value;
+        }
+
+        public double Pressure
+        {
+            get => this.pressure;
+            set => this.pressure = value;
+        }
+    }
+}
