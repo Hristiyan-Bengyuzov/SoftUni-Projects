f24e470678a159f0a4ff8250a24b014624a4eaae
Hris <xrisko06@abv.bg>
2023-02-05 21:55:03 +0200

    added project

---
+++ C# Development/C# Advanced/Defining Classes/Lab/03. Car Constructors/Car.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public Car()
+        {
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+            this.FuelQuantity = 200;
+            this.FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public string Make
+        {
+            get => this.make;
+            set => this.make = value;
+        }
+
+        public string Model
+        {
+            get => this.model;
+            set => this.model = value;
+        }
+
+        public int Year
+        {
+            get => this.year;
+            set => this.year = value;
+        }
+
+        public double FuelQuantity
+        {
+            get => this.fuelQuantity;
+            set => this.fuelQuantity = value;
+        }
+
+        public double FuelConsumption
+        {
+            get => this.fuelConsumption;
+            set => this.fuelConsumption = value;
+        }
+
+        public void Drive(double distance)
+        {
+            if (this.FuelQuantity - (this.FuelConsumption * distance) >= 0)
+            {
+                this.FuelQuantity -= this.FuelConsumption * distance;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.AppendLine($"Fuel: {this.FuelQuantity:F2}");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
