eb0c964d90665f0e32bcc8f6e59fc64966de8c58
Hris <xrisko06@abv.bg>
2023-02-05 21:54:44 +0200

    added project

---
+++ C# Development/C# Advanced/Defining Classes/Lab/01. Car/Car.cs
@@ -0,0 +1,27 @@
+﻿namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+
+        public string Make
+        {
+            get => this.make;
+            set => this.make = value;
+        }
+
+        public string Model
+        {
+            get => this.model;
+            set => this.model = value;
+        }
+
+        public int Year
+        {
+            get => this.year;
+            set => this.year = value;
+        }
+    }
+}
