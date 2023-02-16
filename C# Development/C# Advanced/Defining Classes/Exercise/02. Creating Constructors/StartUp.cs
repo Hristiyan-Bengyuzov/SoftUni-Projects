7f9f1fa92b8b0706a20c2d393ddf61e45f38c649
Hris <xrisko06@abv.bg>
2023-02-16 21:56:39 +0200

    added project

---
+++ C# Development/C# Advanced/Defining Classes/Exercise/02. Creating Constructors/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person firstPerson = new Person();
+            Person secondPerson = new Person(15);
+            Person thirdPerson = new Person("Gosho", 20);
+
+            Console.WriteLine(firstPerson);
+            Console.WriteLine(secondPerson);
+            Console.WriteLine(thirdPerson);
+        }
+    }
+}
