f384d1347729b0fd0637690ee1cd2dd4dff4fcc8
Hris <xrisko06@abv.bg>
2023-02-16 21:57:13 +0200

    added project

---
+++ C# Development/C# Advanced/Defining Classes/Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        public int SubtractDates(string first, string second)
+        {
+            DateTime firstDate = DateTime.Parse(first);
+            DateTime secondDate = DateTime.Parse(second);
+
+            return Math.Abs((firstDate - secondDate).Days);
+        }
+    }
+}
---
+++ C# Development/C# Advanced/Defining Classes/Exercise/05. Date Modifier/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DateModifier dateModifier = new DateModifier();
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+
+            Console.WriteLine(dateModifier.SubtractDates(first, second));
+        }
+    }
+}
