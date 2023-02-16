74b955521363ad3b649cfe9bb34351c556214790
Hris <xrisko06@abv.bg>
2023-02-16 21:57:22 +0200

    added project

---
+++ C# Development/C# Advanced/Defining Classes/Exercise/06. Speed Racing/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            this.TravelledDistance = 0;
+        }
+
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionPerKilometer { get; set; }
+        public double TravelledDistance { get; set; }
+
+        public void Drive(double distance)
+        {
+            double fuelToSpend = distance * this.FuelConsumptionPerKilometer;
+
+            if (this.FuelAmount >= fuelToSpend)
+            {
+                this.FuelAmount -= fuelToSpend;
+                this.TravelledDistance += distance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+        public override string ToString() => $"{this.Model} {this.FuelAmount:f2} {this.TravelledDistance}"; 
+    }
+}
---
+++ C# Development/C# Advanced/Defining Classes/Exercise/06. Speed Racing/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Car> cars = new Dictionary<string, Car>();
+            int countOfCars = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countOfCars; i++)
+            {
+                var carInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string model = carInfo[0];
+
+                if (!cars.ContainsKey(model))
+                {
+                    cars.Add(model, new Car(model, double.Parse(carInfo[1]), double.Parse(carInfo[2])));
+                }
+            } // adding cars to dictionary
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                var tokens = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string model = tokens[1];
+                double distance = double.Parse(tokens[2]);
+
+                if (tokens[0] == "Drive")
+                {
+                    cars[model].Drive(distance);
+                }
+
+                command = Console.ReadLine();
+            } // main logic
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Value);
+            } // printing
+        }
+    }
+}
