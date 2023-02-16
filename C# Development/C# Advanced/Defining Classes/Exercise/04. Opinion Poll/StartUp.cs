e3375f381acf8743a3f5b4137bb20825f3623f0c
Hris <xrisko06@abv.bg>
2023-02-16 21:57:04 +0200

    added project

---
+++ C# Development/C# Advanced/Defining Classes/Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            int countOfPeople = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countOfPeople; i++)
+            {
+                var personInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                people.Add(new Person(personInfo[0], int.Parse(personInfo[1])));
+            }
+
+            foreach (var person in people.Where(p => p.Age > 30).OrderBy(p => p.Name))
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
