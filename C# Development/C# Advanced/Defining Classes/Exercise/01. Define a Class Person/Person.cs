3698526bdb0a8bf7fc193265678fac4106645311
Hris <xrisko06@abv.bg>
2023-02-16 21:56:25 +0200

    added project

---
+++ C# Development/C# Advanced/Defining Classes/Exercise/01. Define a Class Person/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+
+        } // empty constructor
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            set => this.name = value;
+        }
+
+        public int Age
+        {
+            get => this.age;
+            set => this.age = value;
+        }
+
+        public override string ToString() => $"{this.Name} is {this.Age} years old.";
+    }
+}
---
+++ C# Development/C# Advanced/Defining Classes/Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person firstPerson = new Person();
+            firstPerson.Name = "Peter";
+            firstPerson.Age = 20;
+
+            Person secondPerson = new Person
+            {
+                Name = "George",
+                Age = 18
+            };
+
+            Person thirdPerson = new Person("Jose", 43);
+
+            Console.WriteLine(firstPerson);
+            Console.WriteLine(secondPerson);
+            Console.WriteLine(thirdPerson);
+        }
+    }
+}
