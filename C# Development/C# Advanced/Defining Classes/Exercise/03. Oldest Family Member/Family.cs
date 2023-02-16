6738131dd2aebb4006ba81a8dbc123e148370df2
Hris <xrisko06@abv.bg>
2023-02-16 21:56:55 +0200

    added project

---
+++ C# Development/C# Advanced/Defining Classes/Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> members;
+
+        public Family()
+        {
+            this.Members = new List<Person>();
+        }
+
+        public List<Person> Members
+        {
+            get => this.members;
+            set => this.members = value;
+        }
+
+        public void AddMember(Person member) => this.Members.Add(member);
+
+        public Person GetOldestMember() => this.Members.OrderByDescending(x => x.Age).First();
+    }
+}
---
+++ C# Development/C# Advanced/Defining Classes/Exercise/03. Oldest Family Member/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Family family = new Family();
+            int countOfPeople = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countOfPeople; i++)
+            {
+                var personInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                family.AddMember(new Person(personInfo[0], int.Parse(personInfo[1])));
+            }
+
+            Console.WriteLine(family.GetOldestMember());
+        }
+    }
+}
