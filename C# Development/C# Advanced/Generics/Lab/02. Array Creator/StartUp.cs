47a97d7a7cb5f707b7ac0dc3112bf9d1ebcf8887
Hris <xrisko06@abv.bg>
2023-02-25 18:58:14 +0200

    added project

---
+++ C# Development/C# Advanced/Generics/Lab/02. Array Creator/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = ArrayCreator.Create(10, 1); // returns 1 10 times
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
