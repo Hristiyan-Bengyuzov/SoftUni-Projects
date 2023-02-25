c0c777e1b81d50a76d04f6cab655ce8f7d157d84
Hris <xrisko06@abv.bg>
2023-02-25 18:58:08 +0200

    added project

---
+++ C# Development/C# Advanced/Generics/Lab/01. Box/Box.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> stack;
+
+        public Box()
+        {
+            this.stack = new Stack<T>();
+        }
+
+        public void Add(T item) => this.stack.Push(item);
+
+        public T Remove() => this.stack.Pop();
+
+        public int Count => this.stack.Count;
+    }
+}
