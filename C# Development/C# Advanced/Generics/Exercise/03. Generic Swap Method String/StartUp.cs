b393779651e80ab9de1743ea557a3c29f446d250
Hris <xrisko06@abv.bg>
2023-03-04 23:36:35 +0200

    added project

---
+++ C# Development/C# Advanced/Generics/Exercise/03. Generic Swap Method String/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodString
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            List<string> items = new List<string>();
+            int numberOfItems = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                items.Add(Console.ReadLine());
+            }
+
+            int[] indices = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<string> swappedList = Swap(items, indices[0], indices[1]);
+
+            foreach (var item in swappedList)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+        }
+
+        public static List<T> Swap<T>(List<T> items, int firstIndex, int secondIndex)
+        {
+            T temp = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = temp;
+            return items;
+        }
+    }
+}
