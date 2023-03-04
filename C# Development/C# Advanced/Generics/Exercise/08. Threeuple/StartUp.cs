afcdec6f34404ff22332d9d2ccf6a6407682914f
Hris <xrisko06@abv.bg>
2023-03-04 23:37:12 +0200

    added project

---
+++ C# Development/C# Advanced/Generics/Exercise/08. Threeuple/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Threeuple
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split();
+            string fullName = string.Join(' ', input.Take(2));
+            string address = input[2];
+            string town = string.Join(' ', input.Skip(3));
+            var threeuple1 = new Threeuple<string, string, string>(fullName, address, town);
+
+            input = Console.ReadLine().Split();
+            string drunkName = input[0];
+            int litersOfBeer = int.Parse(input[1]);
+            bool isDrunk = input[2] == "drunk" ? true : false;
+            var threeuple2 = new Threeuple<string, int, bool>(drunkName, litersOfBeer, isDrunk);
+
+            input = Console.ReadLine().Split();
+            string name = input[0];
+            double balance = double.Parse(input[1]);
+            string bankName = input[2];
+            var threeuple3 = new Threeuple<string, double, string>(name, balance, bankName);
+
+            Console.WriteLine(threeuple1);
+            Console.WriteLine(threeuple2);
+            Console.WriteLine(threeuple3);
+        }
+    }
+}
---
+++ C# Development/C# Advanced/Generics/Exercise/08. Threeuple/Threeuple.cs
@@ -0,0 +1,22 @@
+﻿namespace Threeuple
+{
+    public class Threeuple<T1, T2, T3>
+    {
+        private T1 first;
+        private T2 second;
+        private T3 third;
+
+        public T1 First { get => this.first; set => this.first = value; }
+        public T2 Second { get => this.second; set => this.second = value; }
+        public T3 Third { get => this.third; set => this.third = value; }
+
+        public Threeuple(T1 first, T2 second, T3 third)
+        {
+            this.First = first;
+            this.Second = second;
+            this.Third = third;
+        }
+
+        public override string ToString() => $"{this.First} -> {this.Second} -> {this.Third}";
+    }
+}
