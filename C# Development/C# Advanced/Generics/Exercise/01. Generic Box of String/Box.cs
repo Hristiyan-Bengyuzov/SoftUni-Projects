8b11859c521ed57c3c9b8b1c9532a4c95e3f58f9
Hris <xrisko06@abv.bg>
2023-03-04 23:36:18 +0200

    added project

---
+++ C# Development/C# Advanced/Generics/Exercise/01. Generic Box of String/Box.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private T boxValue;
+
+        public T BoxValue { get => this.boxValue; set => this.boxValue = value; }
+
+        public Box(T entry)
+        {
+            this.boxValue = entry;
+        }
+
+        public override string ToString() => $"{this.BoxValue.GetType()}: {this.BoxValue}";
+    }
+}
