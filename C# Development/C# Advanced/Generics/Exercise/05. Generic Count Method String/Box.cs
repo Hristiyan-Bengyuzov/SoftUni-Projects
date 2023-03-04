6c2322f440c98147ef8833983ab6ca852978cab4
Hris <xrisko06@abv.bg>
2023-03-04 23:36:50 +0200

    added project

---
+++ C# Development/C# Advanced/Generics/Exercise/05. Generic Count Method String/Box.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericCountMethodString
+{
+    public class Box<T>
+    {
+        private T boxValue;
+
+        public T BoxValue { get => this.boxValue; set => this.boxValue = value; }
+
+        public Box(T entry)
+        {
+            this.boxValue = entry;
+        }
+
+        public static int GetCountOfLargerElements(List<Box<T>> list, T item) => list.Count(x => Comparer<T>.Default.Compare(x.BoxValue, item) > 0);
+    }
+}
---
+++ C# Development/C# Advanced/Generics/Exercise/05. Generic Count Method String/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodString
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            List<Box<string>> boxes = new List<Box<string>>();
+            int numberOfEntries = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfEntries; i++)
+            {
+                boxes.Add(new Box<string>(Console.ReadLine()));
+            }
+
+            string item = Console.ReadLine();
+            Console.WriteLine(Box<string>.GetCountOfLargerElements(boxes, item));
+        }
+    }
+}
