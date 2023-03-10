78fbb0a1a37bea68a34c6412421af1b4799f91a2
Hris <xrisko06@abv.bg>
2023-03-10 23:50:02 +0200

    added project

---
+++ C# Development/C# Advanced/Iterators and Comparators/Lab/04. BookComparer/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = new List<string>(authors);
+        }
+
+        public override string ToString() => $"{this.Title} - {this.Year}";
+        public int CompareTo([AllowNull] Book other) => new BookComparator().Compare(this, other);
+    }
+}
