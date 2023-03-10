4a2d9f2f602fade2d121926bd7dcd09681acaad5
Hris <xrisko06@abv.bg>
2023-03-10 23:49:42 +0200

    added project

---
+++ C# Development/C# Advanced/Iterators and Comparators/Lab/01. Library/Library.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    class Library
+    {
+        public List<Book> Books { get; set; }
+
+        public Library(params Book[] books)
+        {
+            this.Books = new List<Book>(books);
+        }
+    }
+}
