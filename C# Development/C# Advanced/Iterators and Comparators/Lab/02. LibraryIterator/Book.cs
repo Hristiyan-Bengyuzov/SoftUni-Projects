20f9770ea44ab651eae96733990542dacc761955
Hris <xrisko06@abv.bg>
2023-03-10 23:49:49 +0200

    added project

---
+++ C# Development/C# Advanced/Iterators and Comparators/Lab/02. LibraryIterator/Book.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = new List<string>(authors);
+        }
+
+        public override string ToString() => $"{this.Title} - {this.Year} - {string.Join(", ", this.Authors)}";
+    }
+}
---
+++ C# Development/C# Advanced/Iterators and Comparators/Lab/02. LibraryIterator/Library.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorsAndComparators
+{
+    class Library : IEnumerable<Book>
+    {
+        class LibraryIterator : IEnumerator<Book>
+        {
+            private int currentIndex;
+
+            public List<Book> Books { get; set; }
+            public Book Current => this.Books[this.currentIndex];
+            object IEnumerator.Current => this.Current;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.Reset();
+                this.Books = new List<Book>(books);
+            }
+
+            public void Dispose() { }
+            public bool MoveNext() => ++this.currentIndex < this.Books.Count();
+            public void Reset() => this.currentIndex = -1;
+        }
+
+        public List<Book> Books { get; set; }
+
+        public Library(params Book[] books)
+        {
+            this.Books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator() => new LibraryIterator(this.Books);
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
