10b7064814acabf85766beefe09f890f4f9cc7d8
Hris <xrisko06@abv.bg>
2023-03-22 22:44:15 +0200

    added project

---
+++ C# Development/C# Advanced/Iterators and Comparators/Exercise/07. Custom Comparator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._CustomComparator
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Array.Sort(inputArray, new NumComparer());
+            Console.WriteLine(string.Join(" ", inputArray));
+        }
+    }
+
+    public class NumComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x % 2 == 0 && y % 2 != 0) return -1;
+            if (x % 2 != 0 && y % 2 == 0) return 1;
+            return x.CompareTo(y);
+        }
+    }
+}
