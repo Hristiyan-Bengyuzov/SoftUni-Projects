95c74046f063083007e8d4190d0e93ca77b7c8f2
Hris <xrisko06@abv.bg>
2023-03-22 22:43:29 +0200

    added project

---
+++ C# Development/C# Advanced/Iterators and Comparators/Exercise/01. ListyIterator/ListyIterator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private int currentIndex;
+        private List<T> elements;
+
+        public ListyIterator(params T[] entries)
+        {
+            this.elements = new List<T>(entries);
+            this.currentIndex = 0;
+        }
+
+
+        public bool Move()
+        {
+            if (this.HasNext())
+            {
+                this.currentIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext() => this.currentIndex < this.elements.Count - 1;
+
+        public void Print()
+        {
+            if (this.elements.Count != 0)
+                Console.WriteLine(this.elements[currentIndex]);
+            else
+                Console.WriteLine("Invalid Operation!");
+        }
+    }
+}
