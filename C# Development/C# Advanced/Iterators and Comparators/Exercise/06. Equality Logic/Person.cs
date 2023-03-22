2cc852d223926710e6e63ee41824205d41ff61c7
Hris <xrisko06@abv.bg>
2023-03-22 22:44:06 +0200

    added project

---
+++ C# Development/C# Advanced/Iterators and Comparators/Exercise/06. Equality Logic/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _06.EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+
+            return result != 0 ? result : this.Age.CompareTo(other.Age);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person other = obj as Person;
+
+            if (other == null)
+            {
+                return false;
+            }
+
+            return this.Name == other.Name && this.Age == other.Age;
+        }
+
+        public override int GetHashCode() => this.Name.GetHashCode() + this.Age.GetHashCode();
+    }
+}
