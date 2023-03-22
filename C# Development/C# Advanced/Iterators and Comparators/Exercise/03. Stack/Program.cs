939cfae0f952ea175750794ac33009bd9ef4a53b
Hris <xrisko06@abv.bg>
2023-03-22 22:43:44 +0200

    added project

---
+++ C# Development/C# Advanced/Iterators and Comparators/Exercise/03. Stack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Stack
+{
+    class Program
+    {
+        static void Main()
+        {
+            Stack<string> stack = new Stack<string>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] command = input
+                    .Split(new string[] { " ", ", " }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (command[0] == "Push")
+                    stack.Push(command.Skip(1).ToArray());
+                else if (command[0] == "Pop")
+                    stack.Pop();
+            }
+
+            foreach (var item in stack)
+                Console.WriteLine(item);
+
+            foreach (var item in stack)
+                Console.WriteLine(item);
+        }
+    }
+}
---
+++ C# Development/C# Advanced/Iterators and Comparators/Exercise/03. Stack/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _03.Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private List<T> items;
+
+        public Stack()
+        {
+            this.items = new List<T>();
+        }
+
+        public void Push(params T[] elements) => this.items.AddRange(elements);
+       
+        public T Pop()
+        {
+            if (this.items.Count == 0)
+            {
+                Console.WriteLine("No elements");
+                return default(T);
+            }
+            else
+            {
+                T element = this.items[this.items.Count - 1];
+                this.items.RemoveAt(this.items.Count - 1);
+                return element;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.items.Count - 1; i >= 0; i--)
+            {
+                yield return this.items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
