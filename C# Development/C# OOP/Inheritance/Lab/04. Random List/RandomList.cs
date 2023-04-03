6275384634e40118496ae474ba26253025271166
Hris <xrisko06@abv.bg>
2023-04-03 23:11:48 +0300

    added project

---
+++ C# Development/C# OOP/Inheritance/Lab/04. Random List/RandomList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            this.random = new Random();
+        }
+
+        public string RandomString()
+        {
+            int index = this.random.Next(Count);
+            string stringToRemove = this[index];
+            this.RemoveAt(index);
+            return stringToRemove;
+        }
+    }
+}
