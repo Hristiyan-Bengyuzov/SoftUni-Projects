bfad52f41cb818f651b2b13dc6c6028a160aaf0c
Hris <xrisko06@abv.bg>
2023-04-03 23:11:31 +0300

    added project

---
+++ C# Development/C# OOP/Inheritance/Lab/02. Multiple Inheritance/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Puppy puppy = new Puppy();
+            puppy.Weep();
+            puppy.Bark();
+            puppy.Eat();
+        }
+    }
+}
