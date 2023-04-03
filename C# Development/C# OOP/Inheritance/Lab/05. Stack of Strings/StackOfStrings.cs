d24a0c13dc0f5588340d7c0749cf7312a1122597
Hris <xrisko06@abv.bg>
2023-04-03 23:11:54 +0300

    added project

---
+++ C# Development/C# OOP/Inheritance/Lab/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty() => this.Count == 0;
+
+        public Stack<string> AddRange(IEnumerable<string> range)
+        {
+            foreach (var item in range)
+            {
+                this.Push(item);
+            }
+
+            return this;
+        }
+    }
+}
---
+++ C# Development/C# OOP/Inheritance/Lab/05. Stack of Strings/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings strings = new StackOfStrings();
+
+            strings.AddRange(new string[] { "1", "2", "3", "4" });
+
+            while (!strings.IsEmpty())
+            {
+                Console.WriteLine(strings.Pop());
+            }
+        }
+    }
+}
