4852feb17b528e9fc8664e2a09f72dca5ca9a2bf
Hris <xrisko06@abv.bg>
2023-04-04 23:12:13 +0300

    added project

---
+++ C# Development/C# OOP/Inheritance/Exercise/01. Person/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace Person
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            Person person = age > 15 ? new Person(name, age) : new Child(name, age);
+            Console.WriteLine(person);
+        }
+    }
+}
