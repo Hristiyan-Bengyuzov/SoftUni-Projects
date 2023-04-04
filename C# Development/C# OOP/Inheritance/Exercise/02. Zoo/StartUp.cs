49f2ae875d4acdd7bcdd6b9d71862b703fc1cb7f
Hris <xrisko06@abv.bg>
2023-04-04 23:12:20 +0300

    added project

---
+++ C# Development/C# OOP/Inheritance/Exercise/02. Zoo/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace Zoo
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Gorilla gorilla = new Gorilla("monke");
+            Snake snake = new Snake("kobrata");
+
+            Console.WriteLine(gorilla.Name);
+            Console.WriteLine(snake.Name);
+        }
+    }
+}
