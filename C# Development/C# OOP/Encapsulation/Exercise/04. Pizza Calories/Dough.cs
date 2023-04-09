474d6af0bdc31de236e7ddf20a5335f1e7de0a45
Hris <xrisko06@abv.bg>
2023-04-09 22:45:16 +0300

    added project

---
+++ C# Development/C# OOP/Encapsulation/Exercise/04. Pizza Calories/Dough.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzaCalories
+{
+    public class Dough
+    {
+        private Dictionary<string, double> flourTypes = new Dictionary<string, double>
+        {
+            {"white", 1.5},
+            {"wholegrain", 1},
+        };
+
+        private Dictionary<string, double> bakingTechniques = new Dictionary<string, double>
+        {
+            {"crispy", 0.9},
+            {"chewy", 1.1},
+            {"homemade", 1},
+        };
+
+        private const int CaloriesPerGram = 2;
+        private string flourType;
+        private string bakingTechnique;
+        private double weight;
+
+        public Dough(string flourType, string bakingTechnique, double weight)
+        {
+            this.FlourType = flourType;
+            this.BakingTechnique = bakingTechnique;
+            this.Weight = weight;
+        }
+
+        public double Calories =>
+            CaloriesPerGram * this.Weight * flourTypes[this.FlourType.ToLower()] *
+            bakingTechniques[this.BakingTechnique.ToLower()];
+
+        public string FlourType
+        {
+            get => this.flourType;
+            private set
+            {
+                if (!flourTypes.ContainsKey(value.ToLower()))
+                {
+                    throw new ArgumentException("Invalid type of dough.");
+                }
+
+                this.flourType = value;
+            }
+        }
+
+        public string BakingTechnique
+        {
+            get => bakingTechnique;
+            private set
+            {
+                if (!bakingTechniques.ContainsKey(value.ToLower()))
+                {
+                    throw new ArgumentException("Invalid type of dough.");
+                }
+
+                bakingTechnique = value;
+            }
+        }
+
+        public double Weight
+        {
+            get => weight;
+            private set
+            {
+                if (value < 1 || value > 200)
+                {
+                    throw new ArgumentException("Dough weight should be in the range [1..200].");
+                }
+
+                weight = value;
+            }
+        }
+    }
+}
---
+++ C# Development/C# OOP/Encapsulation/Exercise/04. Pizza Calories/Topping.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzaCalories
+{
+    public class Topping
+    {
+        private const int CaloriesPerGram = 2;
+
+        private Dictionary<string, double> types = new Dictionary<string, double>
+        {
+            {"meat", 1.2},
+            {"veggies", 0.8},
+            {"cheese", 1.1},
+            {"sauce", 0.9},
+        };
+
+        private string type;
+        private double weight;
+
+        public Topping(string type, double weight)
+        {
+            Type = type;
+            Weight = weight;
+        }
+
+        public double Calories => CaloriesPerGram * this.Weight * types[this.Type.ToLower()];
+        public string Type
+        {
+            get => type;
+            private set
+            {
+                if (!types.ContainsKey(value.ToLower()))
+                {
+                    throw new ArgumentException($"Cannot place {value} on top of your pizza.");
+                }
+
+                type = value;
+            }
+        }
+
+        public double Weight
+        {
+            get => weight;
+            private set
+            {
+                if (value < 1 || value > 50)
+                {
+                    throw new InvalidOperationException($"{this.type} weight should be in the range [1..50].");
+                }
+
+                weight = value;
+            }
+        }
+    }
+}
