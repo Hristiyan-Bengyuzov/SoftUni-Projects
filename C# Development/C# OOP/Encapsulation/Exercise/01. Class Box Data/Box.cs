38328d89e1cb177c75b175f822805eaa115ed3a7
Hris <xrisko06@abv.bg>
2023-04-09 22:44:52 +0300

    added project

---
+++ C# Development/C# OOP/Encapsulation/Exercise/01. Class Box Data/Box.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get => this.length;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public double SurfaceArea() => 2 * this.Length * this.Width + 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+        public double LateralSurfaceArea() => 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+        public double Volume() => this.Length * this.Width * this.Height;
+    }
+}
