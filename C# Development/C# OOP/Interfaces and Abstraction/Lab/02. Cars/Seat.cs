8913956299a8af8865e741ed3b7e873aeb18d0ad
Hris <xrisko06@abv.bg>
2023-04-12 22:41:04 +0300

    added project

---
+++ C# Development/C# OOP/Interfaces and Abstraction/Lab/02. Cars/Seat.cs
@@ -0,0 +1,18 @@
+﻿namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+
+        public string Model { get; private set; }
+        public string Color { get; private set; }
+
+        public string Start() => "Engine start";
+        public string Stop() => "Breaaak!";
+        public override string ToString() => $"{this.Color} Seat {this.Model}";
+    }
+}
---
+++ C# Development/C# OOP/Interfaces and Abstraction/Lab/02. Cars/Tesla.cs
@@ -0,0 +1,20 @@
+﻿namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+
+        public string Model { get; private set; }
+        public string Color { get; private set; }
+        public int Battery { get; private set; }
+
+        public string Start() => "Engine start";
+        public string Stop() => "Breaaak!";
+        public override string ToString() => $"{this.Color} Tesla {this.Model} with {this.Battery} Batteries";
+    }
+}
