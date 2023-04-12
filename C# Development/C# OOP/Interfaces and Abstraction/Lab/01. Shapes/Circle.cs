32af0036e2fc1feb195f134d582dee1fd15d3a4d
Hris <xrisko06@abv.bg>
2023-04-12 22:40:58 +0300

    added project

---
+++ C# Development/C# OOP/Interfaces and Abstraction/Lab/01. Shapes/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radius) => this.Radius = radius;
+
+        public int Radius
+        {
+            get => this.radius;
+            set => this.radius = value;
+        }
+
+        public void Draw()
+        {
+            double rIn = this.radius - 0.4;
+            double rOut = this.radius + 0.4;
+
+            for (double y = this.radius; y >= -this.radius; --y)
+            {
+                for (double x = -this.radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    Console.Write(value >= rIn * rIn && value <= rOut * rOut ? '*' : ' ');
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
