285e886333862ac11831b0bd1905a8b04e399c26
Hris <xrisko06@abv.bg>
2023-04-22 23:35:06 +0300

    added project

---
+++ C# Development/C# OOP/Polymorphism/Lab/03. Shapes/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius
+        {
+            get => this.radius;
+            private set => this.radius = value;
+        }
+
+        public override double CalculateArea() => Math.PI * Math.Pow(this.Radius, 2);
+        public override double CalculatePerimeter() => 2 * Math.PI * this.Radius;
+    }
+}
