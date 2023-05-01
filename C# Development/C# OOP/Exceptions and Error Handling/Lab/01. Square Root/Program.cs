ca66f4ff095f22b164852a62661df7a0db301bad
Hris <xrisko06@abv.bg>
2023-05-01 22:53:59 +0300

    added project

---
+++ C# Development/C# OOP/Exceptions and Error Handling/Lab/01. Square Root/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine(SquareRoot(num));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+
+        public static double SquareRoot(int num)
+        {
+            if (num < 0) throw new ArgumentException("Invalid number.");
+            return Math.Sqrt(num);
+        }
+    }
+}
