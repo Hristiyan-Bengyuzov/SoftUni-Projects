75bcecebf52562425a8a27c0866729dbbccd496a
Hris <xrisko06@abv.bg>
2023-05-06 23:20:52 +0300

    added project

---
+++ C# Development/C# OOP/Reflection and Attributes/Lab/05. Create Attribute/Tracker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+
+            foreach (MethodInfo method in methods)
+            {
+                if (method.CustomAttributes.Any(n => n.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+                    foreach (AuthorAttribute attr in attributes)
+                    {
+                        Console.WriteLine("{0} is written by {1}", method.Name, attr.Name);
+                    }
+                }
+            }
+        }
+    }
+}
