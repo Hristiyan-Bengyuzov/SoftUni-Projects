7c38acc24b0d1cfc8422108f6958fa6303e56353
Hris <xrisko06@abv.bg>
2023-05-12 22:50:51 +0300

    added project

---
+++ C# Development/C# OOP/Reflection and Attributes/Exercise/01. Command Pattern/Engine.cs
@@ -0,0 +1,25 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+
+namespace CommandPattern
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                string result = this.commandInterpreter.Read(input);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
