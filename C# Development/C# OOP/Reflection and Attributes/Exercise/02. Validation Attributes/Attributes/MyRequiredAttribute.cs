5f9fd20d367042753fb221369ce263d4a2c3802f
Hris <xrisko06@abv.bg>
2023-05-12 22:50:57 +0300

    added project

---
+++ C# Development/C# OOP/Reflection and Attributes/Exercise/02. Validation Attributes/Attributes/MyRequiredAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace ValidationAttributes
+{
+    public class MyRequiredAttribute : MyValidationAttribute
+    {
+        public override bool IsValid(object obj) => obj != null;
+    }
+}
---
+++ C# Development/C# OOP/Reflection and Attributes/Exercise/02. Validation Attributes/Validator.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Reflection;
+
+
+namespace ValidationAttributes
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] properties = obj
+                .GetType()
+                .GetProperties()
+                .Where(x => x.GetCustomAttributes(typeof(MyValidationAttribute)).Any())
+                .ToArray();
+
+            foreach (PropertyInfo property in properties)
+            {
+                object value = property.GetValue(obj);
+                MyValidationAttribute attribute = property.GetCustomAttribute<MyValidationAttribute>();
+                if (!attribute.IsValid(value)) return false;
+            }
+
+            return true;
+        }
+    }
+}
