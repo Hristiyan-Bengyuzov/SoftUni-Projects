e06b46d662f0ebb21f9c7493c9268851d1032d4c
Hris <xrisko06@abv.bg>
2024-02-22 23:02:47 +0200

    added project

---
+++ C# Development/MSSQL/Triggers and Transactions/09. Delete Employees.sql
@@ -0,0 +1,12 @@
+CREATE TRIGGER tr_AddEntityToDeletedEmployeesTable
+ON [Employees] FOR DELETE
+AS
+INSERT INTO [Deleted_Employees]	
+	SELECT		
+		[FirstName],
+		[LastName],
+		[MiddleName],
+		[JobTitle],
+		[DepartmentID],
+		[Salary]
+	FROM deleted
